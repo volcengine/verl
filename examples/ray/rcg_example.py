4ecbfc9ad79d9611e73c3f04793e963b705810db
Amjad Almahairi <anm@anyscale.com>
2024-11-14 14:06:55 -0800

    working example

--- examples/ray/rcg_example.py
+++ examples/ray/rcg_example.py
@@ -22,11 +22,21 @@ class GPUWorker(Worker):
     def __init__(self) -> None:
         super().__init__()
 
-    def mult_by_rank(self, data: DataProto):
+    def mult_x_by_rank(self, data: DataProto):
         data = data.to('cuda')
         res = data.batch['x'] * self.rank
         print(f"rank {self.rank}, shape: {res.shape}")
-        output = DataProto.from_dict(tensors={'values': res})
+        data.pop(batch_keys=['x'])
+        output = data.union(DataProto.from_dict(tensors={'x': res}))
+        return output
+    
+
+    def mult_y_by_rank(self, data: DataProto):
+        data = data.to('cuda')
+        res = data.batch['y'] * self.rank
+        print(f"rank {self.rank}, shape: {res.shape}")
+        data.pop(batch_keys=['y'])
+        output = data.union(DataProto.from_dict(tensors={'y': res}))
         return output
 
 if __name__ == "__main__":
@@ -36,19 +46,28 @@ if __name__ == "__main__":
     worker_group = RayWorkerGroup(resource_pool, class_with_args)
     
     tensors = {
-        'x': torch.ones((256, 512), dtype=torch.int64),
+        'x': torch.ones((256, 512), dtype=torch.int64), 
+        'y': torch.ones((256, 1024), dtype=torch.int64) * 2,
     }   
 
     data_proto = DataProto.from_dict(tensors=tensors)
+
     
     # Standard Ray execution
     splitted_data_proto, _ = dispatch_dp_compute_data_proto(worker_group, data_proto)
-    ray_output = worker_group.execute_all_sync("mult_by_rank", *splitted_data_proto)
-    ray_output = collect_dp_compute_data_proto(worker_group, ray_output)
+    splitted_ray_output = worker_group.execute_all_sync("mult_x_by_rank", *splitted_data_proto)
+    ray_output = collect_dp_compute_data_proto(worker_group, splitted_ray_output)
+    
+    splitted_data_proto, _ = dispatch_dp_compute_data_proto(worker_group, ray_output)
+    splitted_ray_output = worker_group.execute_all_sync("mult_y_by_rank", *splitted_data_proto)
+    ray_output = collect_dp_compute_data_proto(worker_group, splitted_ray_output)
+    print("Ray x:", ray_output.batch['x'].shape, ray_output.batch['x'])
+    print("Ray y:", ray_output.batch['y'].shape, ray_output.batch['y'])
     
     # RCG execution
     with InputNode() as splitted_input:
-        splitted_output = worker_group.bind_all("mult_by_rank", splitted_input)
+        splitted_output = worker_group.bind_all("mult_x_by_rank", splitted_input)
+        splitted_output = worker_group.bind_all("mult_y_by_rank", splitted_output)
         dag = MultiOutputNode(splitted_output)
     dag = dag.experimental_compile()
     
@@ -58,6 +77,6 @@ if __name__ == "__main__":
     splitted_output = ray.get(dag.execute(*splitted_data_proto[0]))
     # concatenate
     rcg_output = collect_dp_compute_data_proto(worker_group, splitted_output)
+    print("RCG x:", rcg_output.batch['x'].shape, rcg_output.batch['x'])
+    print("RCG y:", rcg_output.batch['y'].shape, rcg_output.batch['y'])
 
-    assert torch.allclose(ray_output.batch['values'], rcg_output.batch['values'])
-    print("RCG:", rcg_output.batch['values'].shape, rcg_output.batch['values'], rcg_output.batch['values'].device)
