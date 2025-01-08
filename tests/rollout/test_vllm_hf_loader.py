c37aa783d07473ebce9d80758a222c0e13f09437
shengguangming <shengguangming@bytedance.com>
2025-01-08 19:49:30 +0800

    test l20-0 and torchrun

--- tests/rollout/test_vllm_hf_loader.py
+++ tests/rollout/test_vllm_hf_loader.py
@@ -121,7 +121,6 @@ def test_vllm_with_hf():
     print(f'hf response: {tokenizer.batch_decode(response)}')
     print(f'vllm response: {tokenizer.batch_decode(vllm_output)}')
     assert torch.allclose(response, vllm_output), f'hf_response:{response} | vllm_response:{vllm_output}'
-    assert False
     print('Check Pass')
 
 
