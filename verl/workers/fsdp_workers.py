1e424a7024d7e586c9831b4f0049d62fc2e73962
shengguangming <shengguangming@bytedance.com>
2025-01-17 15:42:13 +0800

    add license and fix build rollout

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -243,8 +243,10 @@ class ActorRolloutRefWorker(Worker):
     def _build_rollout(self):
         from torch.distributed.device_mesh import init_device_mesh
         # TODO(sgm): support FSDP hybrid shard for larger model
-        dp = self.world_size // self.config.tensor_model_parallel_size
-        rollout_device_mesh = init_device_mesh('cuda', mesh_shape=(dp, self.config.tensor_model_parallel_size), mesh_dim_names=['dp', 'infer_tp'])
+        infer_tp = self.config.rollout.tensor_model_parallel_size
+        dp = self.world_size // infer_tp
+        assert self.world_size % infer_tp == 0, f'rollout world_size: {self.world_size} is not divisible by infer_tp: {infer_tp}'
+        rollout_device_mesh = init_device_mesh('cuda', mesh_shape=(dp, infer_tp), mesh_dim_names=['dp', 'infer_tp'])
 
         if self.config.rollout.name == 'hf':
             from verl.workers.rollout import HFRollout
