ef993a6bcd6ab53235bd8780c4d0503eee83c933
shengguangming <shengguangming@bytedance.com>
2025-01-22 18:21:14 +0800

    fix exp name

--- examples/grpo_trainer/run_qwen2-7b.sh
+++ examples/grpo_trainer/run_qwen2-7b.sh
@@ -29,7 +29,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='verl_grpo_example_gsm8k' \
-    trainer.experiment_name='deepseek_llm_7b_function_rm' \
+    trainer.experiment_name='qwen2_7b_function_rm' \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
