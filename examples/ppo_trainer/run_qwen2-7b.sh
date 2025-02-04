024d2cba1504df2d0d0e37abf3cf992c1a5b5c47
Wei Xiong <90632760+WeiXiongUST@users.noreply.github.com>
2025-02-04 16:40:04 -0600

    Update run_qwen2-7b.sh

--- examples/ppo_trainer/run_qwen2-7b.sh
+++ examples/ppo_trainer/run_qwen2-7b.sh
@@ -5,8 +5,8 @@ gsm8k_test_path=$HOME/data/gsm8k/test.parquet
 math_train_path=$HOME/data/math/train.parquet
 math_test_path=$HOME/data/math/test.parquet
 
-train_files="['$gsm8k_train_path', '$math_train_path']"
-test_files="['$gsm8k_test_path', '$math_test_path']"
+train_files="['$math_train_path']"
+test_files="['$math_test_path']"
 
 python3 -m verl.trainer.main_ppo \
     data.train_files="$train_files" \
@@ -15,7 +15,7 @@ python3 -m verl.trainer.main_ppo \
     data.val_batch_size=6312 \
     data.max_prompt_length=1024 \
     data.max_response_length=512 \
-    actor_rollout_ref.model.path=Qwen/Qwen2-7B-Instruct \
+    actor_rollout_ref.model.path=Qwen/Qwen2.5-Math-7B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
@@ -32,7 +32,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     critic.optim.lr=1e-5 \
     critic.model.use_remove_padding=True \
-    critic.model.path=Qwen/Qwen2-7B-Instruct \
+    critic.model.path=Qwen/Qwen2.5-Math-7B \
     critic.model.enable_gradient_checkpointing=True \
     critic.ppo_micro_batch_size_per_gpu=32 \
     critic.model.fsdp_config.param_offload=False \
