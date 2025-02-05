45ef1dfc7150e157c71c3998c636f407ffd0fb40
shengguangming <shengguangming@bytedance.com>
2025-02-05 16:49:35 +0800

    merge digit completion ci with fsdp config

--- tests/e2e/run_ray_trainer.sh
+++ tests/e2e/run_ray_trainer.sh
@@ -10,10 +10,16 @@ rm -rf $OUTPUT_FILE
 python3 tests/e2e/arithmetic_sequence/rl/main_trainer.py \
     data.train_files=tests/e2e/arithmetic_sequence/data/train.parquet \
     data.val_files=tests/e2e/arithmetic_sequence/data/test.parquet \
+    data.train_batch_size=800 \
+    data.val_batch_size=200 \
+    data.max_prompt_length=16 \
+    data.max_response_length=32 \
+    data.return_raw_input_ids=True \
     actor_rollout_ref.model.path=tests/e2e/arithmetic_sequence/model \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=200 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=200 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=200 \
+    actor_rollout_ref.rollout.name=hf \
     critic.ppo_micro_batch_size_per_gpu=200 \
     critic.model.path=tests/e2e/arithmetic_sequence/model | tee $OUTPUT_FILE;
 
