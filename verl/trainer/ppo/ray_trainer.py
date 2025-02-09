cd88a19da1fc2945c799dac6a2e3e423f74b8903
shengguangming <shengguangming@bytedance.com>
2025-02-09 21:59:28 +0800

    lint

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -504,9 +504,10 @@ class RayPPOTrainer(object):
 
         if generations_to_log == 0:
             return
-        
+
         if generations_to_log > 0 and 'wandb' not in self.config.trainer.logger:
-            print('WARNING: `val_generations_to_log_to_wandb` is set to a positive value, but no wandb logger is found. ')
+            print(
+                'WARNING: `val_generations_to_log_to_wandb` is set to a positive value, but no wandb logger is found. ')
             return
 
         import wandb
