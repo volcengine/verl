4e0e241ef2b6773168af5ea69d5afc110dff5776
Yue Wu <wuyueholmes@outlook.com>
2025-02-10 19:12:58 +0000

    fix oom

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ python -m verl.trainer.main_ppo \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32000 \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=26000 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
