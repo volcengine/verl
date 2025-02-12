3a0154593ee80e972c9f12234dfbfe56557801a5
Yue Wu <wuyueholmes@outlook.com>
2025-02-12 00:52:37 +0000

    multi-node with optimized reward

---
+++ run_multi.sh
@@ -0,0 +1,81 @@
+git config --global credential.helper cache
+
+pip install pylatexenc
+
+# login to huggingface and wandb
+huggingface-cli login --token $HF_TOKEN --add-to-git-credential
+wandb login $WANDB_TOKEN
+
+ibstatus
+
+nvidia-smi topo -m
+
+set -x
+
+echo "Running on $HOSTNAME"
+echo "NODE_RANK=$NODE_RANK"
+echo "MASTER_ADDR=$MASTER_ADDR"
+echo "MASTER_PORT=$MASTER_PORT"
+echo "AMLT_OUTPUT_DIR=$AMLT_OUTPUT_DIR"
+
+# if node rank is 0, start ray as head
+if [ $NODE_RANK -eq 0 ]; then
+    ray start --head --port=$MASTER_PORT
+    sleep 60
+else
+    # wait for ray head to start
+    sleep 10
+    ray start --address=$MASTER_ADDR:$MASTER_PORT --block
+
+    # finish with a sleep to keep the process alive
+    echo "Ray should have started, sleeping..."
+    sleep infinity
+fi
+
+# export RAY_ADDRESS="http://$MASTER_ADDR:$MASTER_PORT"
+
+# check if ray is running on all nodes
+ray status
+
+# submit the job to ray
+python -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=/mnt/data/data/aimo_math/train.parquet \
+    data.val_files=/mnt/data/data/aimo_math/test.parquet \
+    data.train_batch_size=1024 \
+    data.val_batch_size=1312 \
+    data.max_prompt_length=512 \
+    data.max_response_length=7680 \
+    actor_rollout_ref.model.path=/mnt/models/phi-4 \
+    actor_rollout_ref.actor.optim.lr=4e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=1024 \
+    actor_rollout_ref.actor.use_dynamic_bsz=True \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=26000 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.grad_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.free_cache_engine=True \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.n=32 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name='verl_grpo_aimo_math_dist' \
+    trainer.experiment_name='phi-4-aimo-math-gropo-dist' \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=$NODES \
+    trainer.save_freq=100 \
+    trainer.test_freq=10 \
+    trainer.default_local_dir=$AMLT_OUTPUT_DIR/checkpoints \
+    trainer.total_epochs=30 $@
+
+    # actor_rollout_ref.rollout.kv_cache_dtype="fp8" \
+    # +actor_rollout_ref.actor.optim.eight_bit=True \
