a85d88f035e0ec24fc1f53da44750c614178da72
zhangshulai <zhangshulai@bytedance.com>
2025-02-14 11:19:30 +0800

    quick fix

--- tests/rollout/test_vllm_spmd.py
+++ tests/rollout/test_vllm_spmd.py
@@ -77,7 +77,7 @@ def test_vllm_spmd():
     # Initialize model and token
     local_cache_path = '~/.cache/verl/rlhf'
     local_cache_path = os.path.expanduser(local_cache_path)
-    hdfs_path = 'deepseek-ai/deepseek-llm-7b-chat'
+    hdfs_path = 'Qwen/Qwen2-7B-Instruct'
     from verl.utils.fs import copy_local_path_from_hdfs
     local_model_path = copy_local_path_from_hdfs(src=hdfs_path, cache_dir=local_cache_path)
     tokenizer = AutoTokenizer.from_pretrained(local_model_path)
