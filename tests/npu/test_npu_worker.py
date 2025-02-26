2e0ce697d97025e6793f8b07541109bbe2abf1da
chendong-1998 <chendong136@huawei.com>
2025-02-26 16:23:40 +0800

    fix lint and copyright
    Signed-off-by: chendong-1998 <chendong136@huawei.com>

--- tests/npu/test_npu_worker.py
+++ tests/npu/test_npu_worker.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pytest
 import ray
 import torch
@@ -13,9 +27,10 @@ def ray_cluster_with_npus():
 
 @ray.remote(resources={"NPU": 1})
 class Worker:
+
     def __init__(self):
         pass
-    
+
     def test_torch_npu_avalable(self):
         available = False
         try:
@@ -24,7 +39,7 @@ class Worker:
         except Exception as e:
             pass
         return available
-        
+
 
 def test_torch_npu(ray_cluster_with_npus):
     worker = Worker.remote()
