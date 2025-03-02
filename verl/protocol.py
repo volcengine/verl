c0a6f6822ba53720719fed6f1e848f10a9feb78d
Haibin Lin <haibin.lin@bytedance.com>
2025-03-01 19:36:04 -0800

    upgrade to py311 for docs

--- verl/protocol.py
+++ verl/protocol.py
@@ -84,7 +84,7 @@ def union_tensor_dict(tensor_dict1: TensorDict, tensor_dict2: TensorDict) -> Ten
     return tensor_dict1
 
 
-def union_numpy_dict(tensor_dict1: Dict[str, np.ndarray], tensor_dict2: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+def union_numpy_dict(tensor_dict1: dict[str, np.ndarray], tensor_dict2: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
     for key, val in tensor_dict2.items():
         if key in tensor_dict1:
             assert isinstance(tensor_dict2[key], np.ndarray)
@@ -97,7 +97,7 @@ def union_numpy_dict(tensor_dict1: Dict[str, np.ndarray], tensor_dict2: Dict[str
     return tensor_dict1
 
 
-def list_of_dict_to_dict_of_list(list_of_dict: List[Dict]):
+def list_of_dict_to_dict_of_list(list_of_dict: list[dict]):
     if len(list_of_dict) == 0:
         return {}
     keys = list_of_dict[0].keys()
@@ -148,7 +148,7 @@ def unfold_batch_dim(data: 'DataProto', batch_dims=2):
     return DataProto(batch=tensor, non_tensor_batch=non_tensor_new, meta_info=data.meta_info)
 
 
-def collate_fn(x: List['DataProtoItem']):
+def collate_fn(x: list['DataProtoItem']):
     batch = []
     non_tensor_batch = []
     for data in x:
