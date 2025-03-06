db072e56978f9ac2e8f03999fd6fa24289623ca4
Zefan Wang <wang-zf20@mails.tsinghua.edu.cn>
2025-03-06 10:50:48 +0800

    increase heartbeat timeout

--- recipe/prime/main_prime.py
+++ recipe/prime/main_prime.py
@@ -42,7 +42,11 @@ def main(config):
 def run_prime(config, compute_score=None):
     if not ray.is_initialized():
         # this is for local ray cluster
-        ray.init(runtime_env={'env_vars': {'TOKENIZERS_PARALLELISM': 'true', 'NCCL_DEBUG': 'WARN'}})
+        ray.init(runtime_env={'env_vars': {
+            'TOKENIZERS_PARALLELISM': 'true',
+            'NCCL_DEBUG': 'WARN'
+        }},
+                 _system_config={'num_heartbeats_timeout': 200})
 
     ray.get(main_task.remote(config, compute_score))
 
