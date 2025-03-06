bede0c40cd372ca9737b8a8f8948b35a734ef1e6
Raf-Chen <1878981292@qq.com>
2025-03-06 13:03:08 +0800

    assertion non-posix

--- verl/utils/reward_score/prime_math/__init__.py
+++ verl/utils/reward_score/prime_math/__init__.py
@@ -34,23 +34,33 @@ BAD_SUBSTRINGS = ["^{", "^("]
 BAD_REGEXES = ["\^[0-9]+\^", "\^[0-9][0-9]+"]
 TUPLE_CHARS = "()[]"
 
+
 def timeout(timeout_seconds: int = 8):
     if os.name == "posix":
         import signal
+
         def decorator(func):
+
             def handler(signum, frame):
                 raise TimeoutError("Operation timed out!")
+
             def wrapper(*args, **kwargs):
                 old_handler = signal.getsignal(signal.SIGALRM)
                 signal.signal(signal.SIGALRM, handler)
                 signal.alarm(timeout_seconds)
+
                 try:
                     return func(*args, **kwargs)
                 finally:
                     signal.alarm(0)
                     signal.signal(signal.SIGALRM, old_handler)
+
             return wrapper
+
         return decorator
+    else:
+        raise NotImplementedError(f"Unsupported OS: {os.name}")
+
 
 def _sympy_parse(expr: str):
     """Parses an expression with sympy."""
@@ -224,6 +234,7 @@ def should_allow_eval(expr: str):
             return False
 
     return True
+
 
 @timeout(timeout_seconds=10)
 def are_equal_under_sympy(ground_truth_normalized: str, given_normalized: str):
