bdce5e8dbb2d478f33d84b4f4f21106d2b1d5c93
zhaochenyang20 <zhaochen20@outlook.com>
2025-03-08 17:51:34 +0000

    tp 2

--- examples/ppo_trainer/rollout_callibration.sh
+++ examples/ppo_trainer/rollout_callibration.sh
@@ -29,7 +29,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=8 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=$ENGINE \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.2 \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=24000 \
