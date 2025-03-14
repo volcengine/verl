fcb0db54e981cd10488748595d820f4fa873e7a5
G.O.D <32255912+gameofdimension@users.noreply.github.com>
2025-03-14 23:35:37 +0800

    Update dp_actor.py
    this line is unnecessary can cause error to metric reduce

--- verl/workers/actor/dp_actor.py
+++ verl/workers/actor/dp_actor.py
@@ -325,6 +325,5 @@ class DataParallelPPOActor(BasePPOActor):
 
                 grad_norm = self._optimizer_step()
                 data = {'actor/grad_norm': grad_norm.detach().item()}
-            append_to_dict(metrics, data)
         self.actor_optimizer.zero_grad()
         return metrics
