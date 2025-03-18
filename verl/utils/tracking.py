14e6f15171433214c371ae83bf2757f86f448059
ZeYi Lin <944270057@qq.com>
2025-03-19 02:35:43 +0800

    feat: add verl config

--- verl/utils/tracking.py
+++ verl/utils/tracking.py
@@ -58,9 +58,10 @@ class Tracking(object):
                 swanlab.login(SWANLAB_API_KEY)  # NOTE: previous login information will be overwritten
             swanlab.init(project=project_name,
                          experiment_name=experiment_name,
-                         config=config,
+                         config={"FRAMEWORK": "veRL"},
                          logdir=SWANLAB_LOG_DIR,
                          mode=SWANLAB_MODE)
+            swanlab.config.update(config)
             self.logger["swanlab"] = swanlab
 
         if 'vemlp_wandb' in default_backend:
