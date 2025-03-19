7a7b87b59bd24faffab7fbe8ebfb5b7413f26767
Rui Lu <learningrate1@gmail.com>
2025-03-19 16:10:52 +0000

    gitignore mask scripts

--- scripts/clean_all.sh
+++ scripts/clean_all.sh
@@ -1,3 +1,11 @@
+allrun pkill verl
+allrun pkill resource_tracker
+allrun pkill sglang::scheduler_None
+
 allrun pkill -f -9 verl
 allrun pkill -f -9 resource_tracker
 allrun pkill -f -9 sglang::scheduler_None
+
+# pkill verl
+# pkill resource_tracker
+# pkill sglang::scheduler_None
