81f64e5501934d3a713d233d4e8d4f2bd461dad5
hoshi-hiyouga <hiyouga@buaa.edu.cn>
2025-03-26 18:08:28 +0800

    Update vllm_rollout_spmd.py

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -181,10 +181,10 @@ class vLLMRollout(BaseRollout):
             vllm_inputs = []
             for raw_prompt_ids, multi_modal_data in zip(non_tensor_batch.pop('raw_prompt_ids'),
                                                         non_tensor_batch.pop('multi_modal_data')):
-                vllm_inputs.append({'prompt_token_ids': list(raw_prompt_ids), 'multi_modal_data': multi_modal_data})
+                vllm_inputs.append({'prompt_token_ids': raw_prompt_ids.tolist(), 'multi_modal_data': multi_modal_data})
         else:
             vllm_inputs = [{
-                'prompt_token_ids': list(raw_prompt_ids)
+                'prompt_token_ids': raw_prompt_ids.tolist()
             } for raw_prompt_ids in non_tensor_batch.pop('raw_prompt_ids')]
 
         do_sample = prompts.meta_info.get('do_sample', True)
