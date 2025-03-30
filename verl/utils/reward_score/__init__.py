9e51087e9dc70f112ff7b04253843e86f8a908d5
Haibin Lin <haibin.lin@bytedance.com>
2025-03-30 09:39:53 -0700

    [reward] fix: enable math-verify if available

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -19,16 +19,18 @@ def _default_compute_score(data_source, solution_str, ground_truth, extra_info=N
         from . import gsm8k
         res = gsm8k.compute_score(solution_str, ground_truth)
     elif data_source in ['lighteval/MATH', 'DigitalLearningGmbH/MATH-lighteval']:
-        from . import math
-        res = math.compute_score(solution_str, ground_truth)
+        try:
+            # [Optional] Math-Verify Integration
+            # For enhanced accuracy, consider utilizing Math-Verify (https://github.com/huggingface/Math-Verify).
+            # Note: Math-Verify needs to be manually installed via pip: `pip install math-verify`.
+            import math_verify
+            from . import math_verify
 
-        # [Optional] Math-Verify Integration
-        # For enhanced accuracy, consider utilizing Math-Verify (https://github.com/huggingface/Math-Verify).
-        # Note: Math-Verify needs to be manually installed via pip: `pip install math-verify`.
-        # To use it, override the `compute_score` function with the following implementation:
+            res = math_verify.compute_score(solution_str, ground_truth)
+        except ImportError:
+            from . import math
 
-        # from . import math_verify
-        # res = math_verify.compute_score(solution_str, ground_truth)
+            res = math.compute_score(solution_str, ground_truth)
     elif data_source in [
             'numina_aops_forum', 'numina_synthetic_math', 'numina_amc_aime', 'numina_synthetic_amc', 'numina_cn_k12',
             'numina_olympiads'
