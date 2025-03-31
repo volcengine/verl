a41509dd69130498332d39d56b722d55fcd5d14c
dirtydan <lumengwu@outlook.com>
2025-03-31 22:14:05 +0800

    fix ref bsz for both kl ci

--- tests/e2e/run_qwen_gsm8k_function_rm_both_kl.sh
+++ tests/e2e/run_qwen_gsm8k_function_rm_both_kl.sh
@@ -23,6 +23,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
     critic.optim.lr=1e-5 \
     critic.model.use_remove_padding=True \
     critic.model.path=Qwen/Qwen2.5-0.5B \
