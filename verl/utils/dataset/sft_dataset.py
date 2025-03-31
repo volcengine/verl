11b2917746f0e7a04820ea1f3e319fcbce827db5
HL <linhaibin.eric@gmail.com>
2025-03-30 23:01:30 -0700

    fix default vals

--- verl/utils/dataset/sft_dataset.py
+++ verl/utils/dataset/sft_dataset.py
@@ -41,12 +41,12 @@ class SFTDataset(Dataset):
 
     def __init__(self, parquet_files: Union[str, List[str]], tokenizer, config):
 
-        prompt_key = config.prompt_key
+        prompt_key = config.get('prompt_key', 'prompt')
         prompt_dict_keys = config.get('prompt_dict_keys', None)
-        response_key = config.response_key
+        response_key = config.get('response_key', 'response')
         response_dict_keys = config.get('response_dict_keys', None)
-        max_length = config.max_length
-        truncation = config.truncation
+        max_length = config.get('max_length', 1024)
+        truncation = config.get('truncation', 'error')
 
         assert truncation in ['error', 'left', 'right']
         self.truncation = truncation
