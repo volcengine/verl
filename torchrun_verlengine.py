86ed1576070d7464af96880a382137ae398e52ac
ocss884 <ocss.lin@gmail.com>
2025-03-31 10:33:35 -0700

    update torchrun file

--- torchrun_verlengine.py
+++ torchrun_verlengine.py
@@ -1,6 +1,8 @@
 import os
 import time
+import socket
 import torch
+from contextlib import closing
 
 from torch.distributed.device_mesh import init_device_mesh
 from torch.distributed.fsdp import CPUOffload
@@ -13,54 +15,20 @@ from torch.distributed.fsdp.api import (
 )
 
 from sglang.srt.entrypoints.verl_engine import VerlEngine
-from transformers import AutoTokenizer
+from sglang.srt.utils import broadcast_pyobj
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-def main():
-    assert torch.cuda.is_available(), "CUDA not available"
-    print("Start init")
-    local_rank, rank, world_size = initialize_global_process_group()
-    print(f"RANK{rank}", local_rank, rank, world_size)
-    
-    if rank == 0:
-        print(f"rank 0 init success")
-    
-    model_name = "Qwen/Qwen2-7B-Instruct"
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    
-    dp, tp, pp = 1, 4, 1
-    kwargs = dict(mesh_shape=(dp, tp, pp), mesh_dim_names=["dp", "tp", "pp"])
-    inference_device_mesh_cpu = init_device_mesh("cpu", **kwargs)
-    print(f"{inference_device_mesh_cpu=}")
-    for k in ["TORCHELASTIC_USE_AGENT_STORE"]:
-        if k in os.environ:
-            del os.environ[k]
-    llm = VerlEngine(
-        model_path=model_name,  # use model of same type but different weight to test update_weights
-        dtype="bfloat16",
-        mem_fraction_static=0.5,
-        device_mesh_cpu=inference_device_mesh_cpu["tp"],
-        base_gpu_id=0,
-        gpu_id_step=1,
-        enable_memory_saver=True,
-        # trust_remote_code=True,
-        # dist_init_addr='100.102.11.36:45654',
-        # nnodes=2,
-    )
-    print('call release_memory_occupation', flush=True)
-    llm.release_memory_occupation()
-    print('sleep...', flush=True)
-    time.sleep(3)
-    print('call resume_memory_occupation', flush=True)
-    llm.resume_memory_occupation()
-    
-    sampling_params = dict(
-        temperature=0, top_p=1, n=1, max_new_tokens=16, ignore_eos=True
-    )
-    outputs = llm.generate("Who is the champion of 2022 World cup?", sampling_params=sampling_params)
+def get_local_ip():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    return local_ip
 
-    if torch.distributed.get_rank() == 0:
-        print(f'SGlang response: {outputs["text"]}')
-    
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
 def initialize_global_process_group(timeout_second=36000):
     from datetime import timedelta
 
@@ -78,8 +46,118 @@ def initialize_global_process_group(timeout_second=36000):
         torch.cuda.set_device(local_rank)
     return local_rank, rank, world_size
 
-# torchrun --nnodes=2 --nproc_per_node=2 --master_addr=100.102.11.36 --master_port=34567 verlengine_multinode.py
-# torchrun --nnodes=2 --nproc_per_node=2 --rdzv_id=4567 --rdzv_endpoint='100.102.11.36:34567' verlengine_multinode.py
-if __name__ == "__main__":
+def main():
+    assert torch.cuda.is_available(), "CUDA not available"
+    print("Start init")
+    local_rank, rank, world_size = initialize_global_process_group()
+    print(f"RANK{rank}", local_rank, rank, world_size)
+    dp, tp, pp = 1, 4, 1
+    # dp, tp, pp = 2, 4, 1
     
+    if rank == 0:
+        print(f"rank 0 init success")
+    
+    model_name = "Qwen/Qwen2-7B-Instruct"
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    
+    "======================================================================================="
+    device_mesh = init_device_mesh(
+        "cuda", mesh_shape=(dp*tp,), mesh_dim_names=["fsdp"]
+    )
+    mixed_precision = MixedPrecision(
+        param_dtype=torch.bfloat16,
+        reduce_dtype=torch.float32,
+        buffer_dtype=torch.float32,
+    )
+    # with torch.device("cuda"):
+    #     actor_model = AutoModelForCausalLM.from_pretrained(
+    #         model_name, trust_remote_code=True
+    #     )
+    #     actor_model.to(torch.bfloat16)
+    # fsdp_model = FSDP(
+    #     actor_model,
+    #     use_orig_params=True,
+    #     auto_wrap_policy=None,
+    #     device_id=torch.cuda.current_device(),
+    #     sharding_strategy=ShardingStrategy.FULL_SHARD,
+    #     mixed_precision=mixed_precision,
+    #     cpu_offload=CPUOffload(offload_params=False),
+    #     sync_module_states=False,
+    #     device_mesh=device_mesh,
+    # )
+    # FSDP.set_state_dict_type(
+    #     fsdp_model,
+    #     state_dict_type=StateDictType.SHARDED_STATE_DICT,
+    #     state_dict_config=ShardedStateDictConfig(),
+    # )
+
+    # state_dict = fsdp_model.state_dict()
+    "======================================================================================="
+    
+    kwargs = dict(
+        mesh_shape=(dp, tp, pp), 
+        mesh_dim_names=["dp", "tp", "pp"]
+    )
+    inference_device_mesh_cpu = init_device_mesh("cpu", **kwargs)
+    tp_rank = inference_device_mesh_cpu["tp"].get_local_rank()
+
+    # NOTE: for each dp we have a TP0, within a TP group we need to have TP0's worker ip:port for 
+    (ip, port) = (get_local_ip(), find_free_port()) if tp_rank == 0 else (None, None)
+    # port = find_free_port() if tp_rank == 0 and dp_rank==0 else None
+    [ip, port] = broadcast_pyobj(
+        [ip, port],
+        rank=tp_rank,
+        dist_group=inference_device_mesh_cpu.get_group("tp"),
+        src=inference_device_mesh_cpu["tp"].mesh[0].item()
+    )
+    
+    # floor
+    world_size = int(os.environ["WORLD_SIZE"])
+    tp_size = tp
+    device_count = 2
+    nnodes = -(-tp_size // device_count)
+    print(f"RANK{rank}: {ip}:{port} nnodes:{nnodes}")
+    
+    for k in ["TORCHELASTIC_USE_AGENT_STORE"]:
+        if k in os.environ:
+            del os.environ[k]
+    
+    print("Building VerlEngine...")
+    llm = VerlEngine(
+        model_path=model_name,  # use model of same type but different weight to test update_weights
+        dtype="bfloat16",
+        mem_fraction_static=0.3,
+        device_mesh_cpu=inference_device_mesh_cpu["tp"],
+        base_gpu_id=0,
+        gpu_id_step=1,
+        enable_memory_saver=True,
+        trust_remote_code=True,
+        dist_init_addr=f'{ip}:{port}',
+        nnodes=nnodes,
+    )
+    print('call release_memory_occupation', flush=True)
+    llm.release_memory_occupation()
+    print('sleep...', flush=True)
+    time.sleep(3)
+    print('call resume_memory_occupation', flush=True)
+    llm.resume_memory_occupation()
+
+    print("updating rollout weights")
+    # llm.update_weights_from_tensor([(k, v) for k, v in state_dict.items()])
+
+    "=============================== Gen ==============================="
+    sampling_params = dict(
+        temperature=0, top_p=1, n=1, max_new_tokens=16, ignore_eos=True
+    )
+    outputs = llm.generate("Introduce yourself.", sampling_params=sampling_params)
+
+    if torch.distributed.get_rank() == 0:
+        print("="*64)
+        print(f'SGlang response: {outputs["text"]}')
+        print("="*64)
+    "=============================== Gen ==============================="
+    
+
+# torchrun --nnodes=2 --nproc_per_node=2 --master_addr=<NODE0 IP> --master_port=34567 --node_rank 0 torchrun_verlengine.py
+if __name__ == "__main__":
     main()
