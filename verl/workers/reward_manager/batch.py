b2bb74c438bcd09a267d275bf0247e560cccccb9
mertunsall <mertunsal1905@gmail.com>
2025-04-03 04:43:02 +0800

    update year

--- verl/workers/reward_manager/batch.py
+++ verl/workers/reward_manager/batch.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Mert Unsal
+# Copyright 2025 Mert Unsal
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
