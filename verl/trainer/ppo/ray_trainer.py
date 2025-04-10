1fd5a02645ff9e1f50553da657b60634b588344e
Taiwei Shi <39599206+maksimstw@users.noreply.github.com>
2025-04-10 16:43:50 -0700

    Update ray_trainer.py

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -426,7 +426,7 @@ class RayPPOTrainer(object):
         if self.config.data.adarft.enable:
             # Use CurriculumSampler
             from .custom_sampler import CurriculumSampler
-            sampler = CurriculumSampler(
+            self.sampler = CurriculumSampler(
                 data_source=self.train_dataset,
                 batch_size=self.config.data.train_batch_size,
                 target_difficulty=0
@@ -434,7 +434,7 @@ class RayPPOTrainer(object):
             self.train_dataloader = StatefulDataLoader(dataset=self.train_dataset,
                                                        num_workers=8,
                                                        collate_fn=collate_fn,
-                                                       batch_sampler=sampler)
+                                                       batch_sampler=self.sampler)
         else:
             elif self.config.data.shuffle:
                 train_dataloader_generator = torch.Generator()
