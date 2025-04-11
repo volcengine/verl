efb69942b27c25093826bf06758b79451fe15faa
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-04-11 09:52:45 +0000

    temp: test CI

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -15,7 +15,7 @@
 FSDP PPO Trainer with Ray-based single controller.
 This trainer supports model-agonistic model initialization with huggingface
 """
-
+# test CI
 import os
 import uuid
 from contextlib import contextmanager
