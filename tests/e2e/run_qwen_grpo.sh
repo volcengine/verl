50c6d0dfb3e7aa19bd093c9fcd44a9c24d96e867
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-04-11 18:47:38 +0000

    feat: 2 GPUs test

--- tests/e2e/run_qwen_grpo.sh
+++ tests/e2e/run_qwen_grpo.sh
@@ -35,8 +35,8 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger=['console'] \
     trainer.project_name='verl_grpo_example_gsm8k' \
-    trainer.experiment_name='qwen2_7b_function_rm' \
-    trainer.n_gpus_per_node=8 \
+    trainer.experiment_name='qwen2p5_0p5b_function_rm' \
+    trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
     trainer.test_freq=5 \
