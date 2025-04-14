ab0c15991e28e0a1f225cc4fb2e370097cb6b43c
hoshi-hiyouga <hiyouga@buaa.edu.cn>
2025-04-15 00:21:35 +0800

    Update rl_dataset.py

--- verl/utils/dataset/rl_dataset.py
+++ verl/utils/dataset/rl_dataset.py
@@ -226,7 +226,7 @@ class RLHFDataset(Dataset):
         row_dict['raw_prompt_ids'] = raw_prompt_ids
         # encode prompts without chat template
         if self.return_raw_chat:
-            row_dict['raw_prompt'] = raw_prompt
+            row_dict['raw_prompt'] = messages
 
         # add index for each prompt
         index = row_dict.get("extra_info", {}).get("index", 0)
