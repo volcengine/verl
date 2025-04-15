5ea932436c175e9f71132195093b36b5b56009df
gaoziyuan <gaoziyuan.955@bytedance.com>
2025-04-15 20:56:19 +0800

    format

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -549,7 +549,10 @@ class ActorRolloutRefWorker(Worker):
             data = self.ulysses_sharding_manager.preprocess_data(data)
             output, metrics = self.actor.compute_log_prob(data=data)
             output = DataProto.from_dict(tensors={'old_log_probs': output},
-                                         meta_info={'temperature': self.config.rollout.temperature, 'metrics': metrics})
+                                         meta_info={
+                                             'temperature': self.config.rollout.temperature,
+                                             'metrics': metrics
+                                         })
             output = self.ulysses_sharding_manager.postprocess_data(output)
 
         output = output.to('cpu')
