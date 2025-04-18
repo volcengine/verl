222bf315d70b57b7a47a9e861dcae864867ab63a
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-04-18 11:14:13 +0000

    feat: immigrate to ruff and pre-commit

--- verl/workers/rollout/tokenizer.py
+++ verl/workers/rollout/tokenizer.py
@@ -14,10 +14,11 @@
 """
 The base tokenizer class, required for any hybrid engine based rollout or inference with vLLM.
 """
+
 from abc import ABC, abstractmethod
 from typing import Dict, List, Union
 
-__all__ = ['HybridEngineBaseTokenizer']
+__all__ = ["HybridEngineBaseTokenizer"]
 
 
 class HybridEngineBaseTokenizer(ABC):
@@ -113,9 +114,9 @@ class HybridEngineBaseTokenizer(ABC):
         pass
 
     @abstractmethod
-    def convert_ids_to_tokens(self,
-                              ids: Union[int, List[int]],
-                              skip_special_tokens: bool = False) -> Union[str, List[str]]:
+    def convert_ids_to_tokens(
+        self, ids: Union[int, List[int]], skip_special_tokens: bool = False
+    ) -> Union[str, List[str]]:
         """
         Converts a single index or a sequence of indices in a token or a sequence of tokens, using the vocabulary and
         added tokens.
