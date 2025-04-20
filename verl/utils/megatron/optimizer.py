e214f6afefef527bbd8a0ea76cae4696911d9219
haibin.lin <haibin.lin@bytedance.com>
2025-04-20 13:53:11 +0800

    fix lint

--- verl/utils/megatron/optimizer.py
+++ verl/utils/megatron/optimizer.py
@@ -14,21 +14,24 @@
 # limitations under the License.
 
 from megatron.core.optimizer import OptimizerConfig
-from megatron.core.optimizer_param_scheduler import OptimizerParamScheduler
 from megatron.core.optimizer import get_megatron_optimizer as get_megatron_optimizer_native
 
+
 def get_megatron_optimizer(
-        model,
-        config: OptimizerConfig,
-        no_weight_decay_cond=None,
-        scale_lr_cond=None,
-        lr_mult=1.0,
+    model,
+    config: OptimizerConfig,
+    no_weight_decay_cond=None,
+    scale_lr_cond=None,
+    lr_mult=1.0,
 ):
     # Base optimizer.
-    return get_megatron_optimizer_native(config=config,
-                                         model_chunks=model,
-                                         no_weight_decay_cond=no_weight_decay_cond,
-                                         scale_lr_cond=scale_lr_cond,
-                                         lr_mult=lr_mult)
+    return get_megatron_optimizer_native(
+        config=config,
+        model_chunks=model,
+        no_weight_decay_cond=no_weight_decay_cond,
+        scale_lr_cond=scale_lr_cond,
+        lr_mult=lr_mult,
+    )
+
 
 # TODO: add get_optimizer_param_scheduler(optimizer) to implement lr scheuler.
