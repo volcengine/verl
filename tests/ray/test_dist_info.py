edb36ab8a121f2d4fc83ddbd75f17e578e94b1ec
Chi Zhang <zhangchi.usc1992@bytedance.com>
2025-04-25 20:17:22 +0800

    add license

--- tests/ray/test_dist_info.py
+++ tests/ray/test_dist_info.py
@@ -1,9 +1,32 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Test the global distributed info
 """
 
+import ray
 import torch
-from verl.single_controller.base.worker import DistRankInfo, DistGlobalInfo
+
+from verl import DataProto
+from verl.single_controller.base import Worker
+from verl.single_controller.base.decorator import (
+    make_nd_compute_dataproto_dispatch_fn,
+    make_nd_compute_dispatch_fn,
+    register,
+)
+from verl.single_controller.base.worker import DistGlobalInfo, DistRankInfo
 
 
 def test_dist_global_info():
@@ -12,7 +35,7 @@ def test_dist_global_info():
     result = []
 
     for i in range(dist_info.get_world_size()):
-        result.append(dist_info.get_rank_info(global_rank=i, order='tp-pp-dp-ep-cp'))
+        result.append(dist_info.get_rank_info(global_rank=i, order="tp-pp-dp-ep-cp"))
 
     expected_result = [
         DistRankInfo(tp_rank=0, cp_rank=0, ep_rank=0, dp_rank=0, pp_rank=0),
@@ -53,50 +76,47 @@ def test_dist_global_info():
         assert info == expected_info
 
 
-from verl.single_controller.base import Worker
-from verl.single_controller.base.decorator import register, Dispatch, make_nd_compute_dataproto_dispatch_fn, make_nd_compute_dispatch_fn
-from verl import DataProto
-import ray
-
 @ray.remote
 class TestActor(Worker):
     def __init__(self) -> None:
         super().__init__()
 
         import torch.distributed
-        torch.distributed.init_process_group(backend='nccl')
-        self.infer_device_mesh = torch.distributed.device_mesh.init_device_mesh(device_type='cuda', 
-                                                                                mesh_shape=[2, 4], mesh_dim_names=['dp', 'tp'])
-        self.train_device_mesh = torch.distributed.device_mesh.init_device_mesh(device_type='cuda', 
-                                                                                mesh_shape=[2, 2, 2], mesh_dim_names=['pp', 'dp', 'tp'])
 
+        torch.distributed.init_process_group(backend="nccl")
+        self.infer_device_mesh = torch.distributed.device_mesh.init_device_mesh(
+            device_type="cuda", mesh_shape=[2, 4], mesh_dim_names=["dp", "tp"]
+        )
+        self.train_device_mesh = torch.distributed.device_mesh.init_device_mesh(
+            device_type="cuda", mesh_shape=[2, 2, 2], mesh_dim_names=["pp", "dp", "tp"]
+        )
 
-    @register(dispatch_mode=make_nd_compute_dataproto_dispatch_fn(mesh_name='infer'))
+    @register(dispatch_mode=make_nd_compute_dataproto_dispatch_fn(mesh_name="infer"))
     def generate_data_proto(self, data: DataProto):
-        tp_rank = self.infer_device_mesh['tp'].get_local_rank()
-        dp_rank = self.infer_device_mesh['dp'].get_local_rank()
-        data.batch['a'] += (tp_rank + 1) * dp_rank
-        return data
-    
-    @register(dispatch_mode=make_nd_compute_dispatch_fn(mesh_name='infer'))
-    def generate(self, data: int):
-        tp_rank = self.infer_device_mesh['tp'].get_local_rank()
-        dp_rank = self.infer_device_mesh['dp'].get_local_rank()
-        print(f'tp_rank: {tp_rank}, dp_rank: {dp_rank}, init data: {data}')
-        data += (tp_rank + 1) * dp_rank
-        print(f'tp_rank: {tp_rank}, dp_rank: {dp_rank}, output data: {data}')
+        tp_rank = self.infer_device_mesh["tp"].get_local_rank()
+        dp_rank = self.infer_device_mesh["dp"].get_local_rank()
+        data.batch["a"] += (tp_rank + 1) * dp_rank
         return data
 
-    @register(dispatch_mode=make_nd_compute_dataproto_dispatch_fn(mesh_name='train'))
+    @register(dispatch_mode=make_nd_compute_dispatch_fn(mesh_name="infer"))
+    def generate(self, data: int):
+        tp_rank = self.infer_device_mesh["tp"].get_local_rank()
+        dp_rank = self.infer_device_mesh["dp"].get_local_rank()
+        print(f"tp_rank: {tp_rank}, dp_rank: {dp_rank}, init data: {data}")
+        data += (tp_rank + 1) * dp_rank
+        print(f"tp_rank: {tp_rank}, dp_rank: {dp_rank}, output data: {data}")
+        return data
+
+    @register(dispatch_mode=make_nd_compute_dataproto_dispatch_fn(mesh_name="train"))
     def train_data_proto(self, data: DataProto):
-        tp_rank = self.train_device_mesh['tp'].get_local_rank()
-        dp_rank = self.train_device_mesh['dp'].get_local_rank()
-        pp_rank = self.train_device_mesh['pp'].get_local_rank()
-        data.batch['a'] += (tp_rank + 1) * (dp_rank + 2) * (pp_rank + 3)
+        tp_rank = self.train_device_mesh["tp"].get_local_rank()
+        dp_rank = self.train_device_mesh["dp"].get_local_rank()
+        pp_rank = self.train_device_mesh["pp"].get_local_rank()
+        data.batch["a"] += (tp_rank + 1) * (dp_rank + 2) * (pp_rank + 3)
         # tp rank 0, pp rank 1, dp rank 0, output data added: 8 + 3 = 11
         # tp rank 0, pp rank 1, dp rank 1, output data added: 12 + 4 = 16
         return data
-        
+
 
 def test_dist_global_info_wg():
     # create a worker group with size 8
@@ -111,22 +131,22 @@ def test_dist_global_info_wg():
     resource_pool = RayResourcePool(process_on_nodes=[8])
     wg = RayWorkerGroup(resource_pool=resource_pool, ray_cls_with_init=ray_cls)
 
-    wg.register_dist_info(name='infer', dist_global_info=DistGlobalInfo(tp_size=4, dp_size=2))
-    wg.register_dist_info(name='train', dist_global_info=DistGlobalInfo(tp_size=2, dp_size=2, pp_size=2))
+    wg.register_dist_info(name="infer", dist_global_info=DistGlobalInfo(tp_size=4, dp_size=2))
+    wg.register_dist_info(name="train", dist_global_info=DistGlobalInfo(tp_size=2, dp_size=2, pp_size=2))
 
     infer_input_data = [1, 2]
     infer_output_data = wg.generate(infer_input_data)
 
     assert infer_output_data == [1, 3]
 
-    infer_input_data_proto = DataProto.from_single_dict(data={'a': torch.tensor([1, 2])})
+    infer_input_data_proto = DataProto.from_single_dict(data={"a": torch.tensor([1, 2])})
     infer_output_data_proto = wg.generate_data_proto(infer_input_data_proto)
 
-    assert torch.all(torch.eq(infer_output_data_proto.batch['a'], torch.tensor([1, 3])))
+    assert torch.all(torch.eq(infer_output_data_proto.batch["a"], torch.tensor([1, 3])))
 
-    train_input_data_proto = DataProto.from_single_dict(data={'a': torch.tensor([3, 4])})
+    train_input_data_proto = DataProto.from_single_dict(data={"a": torch.tensor([3, 4])})
     train_output_data_proto = wg.train_data_proto(train_input_data_proto)
 
-    assert torch.all(torch.eq(train_output_data_proto.batch['a'], torch.tensor([11, 16])))
+    assert torch.all(torch.eq(train_output_data_proto.batch["a"], torch.tensor([11, 16])))
 
     ray.shutdown()
