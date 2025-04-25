05e3b08f83ce57cfed8e4e451220f30c17113090
zhangchi.usc1992 <zhangchi.usc1992@bytedance.com>
2025-04-25 19:52:43 +0800

    update test

--- tests/ray/test_dist_info.py
+++ tests/ray/test_dist_info.py
@@ -98,34 +98,35 @@ class TestActor(Worker):
         return data
         
 
-# def test_dist_global_info_wg():
-# create a worker group with size 8
-# register a infer dist info with tp=4, dp=2
-# register a train dist info with tp=2, dp=2, pp=2
-# test the correctness of data dispatch and computation
-from verl.single_controller.ray import RayClassWithInitArgs, RayResourcePool, RayWorkerGroup
+def test_dist_global_info_wg():
+    # create a worker group with size 8
+    # register a infer dist info with tp=4, dp=2
+    # register a train dist info with tp=2, dp=2, pp=2
+    # test the correctness of data dispatch and computation
+    from verl.single_controller.ray import RayClassWithInitArgs, RayResourcePool, RayWorkerGroup
 
+    ray.init()
 
-ray.init()
+    ray_cls = RayClassWithInitArgs(TestActor)
+    resource_pool = RayResourcePool(process_on_nodes=[8])
+    wg = RayWorkerGroup(resource_pool=resource_pool, ray_cls_with_init=ray_cls)
 
-ray_cls = RayClassWithInitArgs(TestActor)
-resource_pool = RayResourcePool(process_on_nodes=[8])
-wg = RayWorkerGroup(resource_pool=resource_pool, ray_cls_with_init=ray_cls)
+    wg.register_dist_info(name='infer', dist_global_info=DistGlobalInfo(tp_size=4, dp_size=2))
+    wg.register_dist_info(name='train', dist_global_info=DistGlobalInfo(tp_size=2, dp_size=2, pp_size=2))
 
-wg.register_dist_info(name='infer', dist_global_info=DistGlobalInfo(tp_size=4, dp_size=2))
-wg.register_dist_info(name='train', dist_global_info=DistGlobalInfo(tp_size=2, dp_size=2, pp_size=2))
+    infer_input_data = [1, 2]
+    infer_output_data = wg.generate(infer_input_data)
 
-infer_input_data = [1, 2]
-infer_output_data = wg.generate(infer_input_data)
+    assert infer_output_data == [1, 3]
 
-assert infer_output_data == [1, 3]
+    infer_input_data_proto = DataProto.from_single_dict(data={'a': torch.tensor([1, 2])})
+    infer_output_data_proto = wg.generate_data_proto(infer_input_data_proto)
 
-infer_input_data_proto = DataProto.from_single_dict(data={'a': torch.tensor([1, 2])})
-infer_output_data_proto = wg.generate_data_proto(infer_input_data_proto)
+    assert torch.all(torch.eq(infer_output_data_proto.batch['a'], torch.tensor([1, 3])))
 
-assert torch.all(torch.eq(infer_output_data_proto.batch['a'], torch.tensor([1, 3])))
+    train_input_data_proto = DataProto.from_single_dict(data={'a': torch.tensor([3, 4])})
+    train_output_data_proto = wg.train_data_proto(train_input_data_proto)
 
-train_input_data_proto = DataProto.from_single_dict(data={'a': torch.tensor([3, 4])})
-train_output_data_proto = wg.train_data_proto(train_input_data_proto)
+    assert torch.all(torch.eq(train_output_data_proto.batch['a'], torch.tensor([11, 16])))
 
-assert torch.all(torch.eq(train_output_data_proto.batch['a'], torch.tensor([11, 16])))
+    ray.shutdown()
