60641831498eb2e74e7c673e2f38046ade6374d1
zhangchi.usc1992 <zhangchi.usc1992@bytedance.com>
2025-04-25 19:50:36 +0800

    add tests

--- tests/ray/test_dist_info.py
+++ tests/ray/test_dist_info.py
@@ -87,14 +87,16 @@ class TestActor(Worker):
         print(f'tp_rank: {tp_rank}, dp_rank: {dp_rank}, output data: {data}')
         return data
 
-    # @register(dispatch_mode=Dispatch.nDProto, mesh='train')
-    # def train(self, data: DataProto):
-    #     tp_rank = self.train_device_mesh['tp'].get_local_rank()
-    #     dp_rank = self.train_device_mesh['dp'].get_local_rank()
-    #     pp_rank = self.train_device_mesh['pp'].get_local_rank()
+    @register(dispatch_mode=make_nd_compute_dataproto_dispatch_fn(mesh_name='train'))
+    def train_data_proto(self, data: DataProto):
+        tp_rank = self.train_device_mesh['tp'].get_local_rank()
+        dp_rank = self.train_device_mesh['dp'].get_local_rank()
+        pp_rank = self.train_device_mesh['pp'].get_local_rank()
+        data.batch['a'] += (tp_rank + 1) * (dp_rank + 2) * (pp_rank + 3)
+        # tp rank 0, pp rank 1, dp rank 0, output data added: 8 + 3 = 11
+        # tp rank 0, pp rank 1, dp rank 1, output data added: 12 + 4 = 16
+        return data
         
-
-
 
 # def test_dist_global_info_wg():
 # create a worker group with size 8
@@ -116,6 +118,14 @@ wg.register_dist_info(name='train', dist_global_info=DistGlobalInfo(tp_size=2, d
 infer_input_data = [1, 2]
 infer_output_data = wg.generate(infer_input_data)
 
+assert infer_output_data == [1, 3]
+
 infer_input_data_proto = DataProto.from_single_dict(data={'a': torch.tensor([1, 2])})
 infer_output_data_proto = wg.generate_data_proto(infer_input_data_proto)
 
+assert torch.all(torch.eq(infer_output_data_proto.batch['a'], torch.tensor([1, 3])))
+
+train_input_data_proto = DataProto.from_single_dict(data={'a': torch.tensor([3, 4])})
+train_output_data_proto = wg.train_data_proto(train_input_data_proto)
+
+assert torch.all(torch.eq(train_output_data_proto.batch['a'], torch.tensor([11, 16])))
