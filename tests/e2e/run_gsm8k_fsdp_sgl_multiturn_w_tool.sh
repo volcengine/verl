db781c051c46dbafddefc58d733362ce5c020863
HL <linhaibin.eric@gmail.com>
2025-04-28 11:09:24 -0700

    fix wandb

--- tests/e2e/run_gsm8k_fsdp_sgl_multiturn_w_tool.sh
+++ tests/e2e/run_gsm8k_fsdp_sgl_multiturn_w_tool.sh
@@ -41,7 +41,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
-    trainer.logger=['console','wandb'] \
+    trainer.logger=['console'] \
     trainer.project_name='gsm8k_async_rl' \
     trainer.experiment_name='qwen2.5-3b_function_rm-gsm8k-async-sgl-multi-w-tool-rebased-0427-verify-n16' \
     trainer.n_gpus_per_node=8 \
