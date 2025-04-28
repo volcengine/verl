4d35415ea96a62c55f230028d6eea3fd146951d4
haibin.lin <haibin.lin@bytedance.com>
2025-04-28 11:17:32 +0800

    fix a bug

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -424,9 +424,9 @@ class RayPPOTrainer:
         from verl.trainer.main_ppo import create_rl_dataset, create_rl_sampler
 
         if train_dataset is None:
-            train_dataset = create_rl_dataset(self.config.data, self.tokenizer, self.processor, self.config.data.train_files)
+            train_dataset = create_rl_dataset(self.config.data.train_files, self.config.data, self.tokenizer, self.processor)
         if val_dataset is None:
-            val_dataset = create_rl_dataset(self.config.data, self.tokenizer, self.processor, self.config.data.val_files)
+            val_dataset = create_rl_dataset(self.config.data.val_files, self.config.data, self.tokenizer, self.processor)
         self.train_dataset, self.val_dataset = train_dataset, val_dataset
 
         if train_sampler is None:
