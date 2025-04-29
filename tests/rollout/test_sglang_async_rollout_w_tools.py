83a735d7db14213b7e4cd5f192ad067b0cad0ed7
Xiang Long <longxiang1@modelbest.cn>
2025-04-29 13:46:36 +0800

    Add sharding manager to unit test

--- tests/rollout/test_sglang_async_rollout_w_tools.py
+++ tests/rollout/test_sglang_async_rollout_w_tools.py
@@ -109,7 +109,13 @@ def test_async_sglang_rollout_w_tool():
             }
         )
 
+        prompts = rollout_sharding_manager.preprocess_data(prompts)
+        # log_gpu_memory_usage("Before generating sequences", logger=None)
         output = rollout.generate_sequences_with_tools(prompts=prompts)
+        print(f"generated {output.batch['responses'].shape=}")
+        # log_gpu_memory_usage("After generating sequences", logger=None)
+        output = rollout_sharding_manager.postprocess_data(output)
+        print(f"postprocessed {output.batch['responses'].shape=}")
         sglang_output = output.to("cpu")
 
     sglang_response_tokens = tokenizer.batch_decode(sglang_output.batch["responses"])
