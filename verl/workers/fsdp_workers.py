66caa467eb9c583336ffe1f116e75ac48fff7db5
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-05-01 04:26:50 +0800

    fix: fsdp_workers and tests

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -1195,7 +1195,7 @@ class RewardModelWorker(Worker):
     def compute_rm_score(self, data: DataProto):
         import itertools
 
-        from verl.utils.seqlen_balancing import get_reverse_idx, rearrange_micro_batches
+        from verl.utils.seqlen_balancing import get_reverse_idx, get_uniform_data_chunks
 
         # Support all hardwares
         data = data.to(torch.cuda.current_device())
@@ -1223,12 +1223,12 @@ class RewardModelWorker(Worker):
             use_dynamic_bsz = self.config.use_dynamic_bsz
             if use_dynamic_bsz:
                 max_token_len = self.config.forward_max_token_len_per_gpu * self.ulysses_sequence_parallel_size
-                micro_batches, indices = rearrange_micro_batches(batch=rm_data.batch, max_token_len=max_token_len)
+                micro_data_chunks, indices = get_uniform_data_chunks(data=rm_data, max_token_len=max_token_len)
             else:
-                micro_batches = rm_data.batch.split(self.config.micro_batch_size_per_gpu)
+                micro_data_chunks = rm_data.split(split_size=self.config.micro_batch_size_per_gpu)
             output = []
-            for micro_batch in micro_batches:
-                rm_score = self._forward_micro_batch(micro_batch)
+            for micro_data_chunk in micro_data_chunks:
+                rm_score = self._forward_micro_batch(micro_batch=micro_data_chunk.batch)
                 output.append(rm_score)
             scores = torch.cat(output, dim=0)  # (batch_size)
 
