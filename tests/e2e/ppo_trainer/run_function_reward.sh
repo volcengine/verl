b873a04fd87c1bca6e341748ef72d989b2a4f550
0x404 <871206929@qq.com>
2025-05-10 11:35:09 +0800

    Update E2E training scripts to include VERL_EXP_NAME

--- tests/e2e/ppo_trainer/run_function_reward.sh
+++ tests/e2e/ppo_trainer/run_function_reward.sh
@@ -64,7 +64,7 @@ EOF
     rm -rf "${output_file}"
 fi
 
-exp_name="$(basename "${MODEL_ID,,}")-function-reward-minimal"
+exp_name="${VERL_EXP_NAME:-$(basename "${MODEL_ID,,}")-function-reward-minimal}"
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator="${ADV_ESTIMATOR}" \
