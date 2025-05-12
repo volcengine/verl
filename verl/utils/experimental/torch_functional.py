35f7763b6fd1838eb977c9d554c94aa5c28bbc24
mingrui.wang2 <mingruimingrui@hotmail.com>
2025-05-12 19:00:15 +0800

    remove accidental imports

--- verl/utils/experimental/torch_functional.py
+++ verl/utils/experimental/torch_functional.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from turtle import backward, forward
-from typing import Optional, Tuple
+from typing import Tuple
 import torch
 
 
