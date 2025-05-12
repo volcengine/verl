b96bcf8c079fee7c8a83f4a9062d607376d9cfb5
mingrui.wang2 <mingruimingrui@hotmail.com>
2025-05-12 18:49:42 +0800

    added fused_entropy_log_probs

--- verl/utils/experimental/torch_functional.py
+++ verl/utils/experimental/torch_functional.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Tuple
+from turtle import backward, forward
+from typing import Optional, Tuple
 import torch
 
 
@@ -315,3 +316,158 @@ def fused_log_probs(
         temperature,
         chunk_size,
     )
+
+
+def fused_entropy_log_probs_fwd(
+    hidden_states: torch.FloatTensor,
+    vocab_weights: torch.FloatTensor,
+    input_ids: torch.LongTensor,
+    temperature: float = 1.0
+) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+    logits = (hidden_states @ vocab_weights.t()) / temperature
+    orig_dtype = logits.dtype
+    logits = logits.to(torch.float32)
+
+    # Slower but more numerically stable to do log_softmax, rather than probs.log()
+    probs = logits.softmax(dim=-1)
+    log_probs = logits.log_softmax(dim=-1)
+
+    entropy = torch.logsumexp(logits, dim=-1) - torch.sum(probs * logits, dim=-1)
+
+    token_log_probs = log_probs.gather(-1, input_ids.unsqueeze(-1)).squeeze(-1)
+
+    return entropy.to(orig_dtype), token_log_probs.to(orig_dtype)
+
+
+def fused_entropy_log_probs_bwd(
+    dentropy: torch.FloatTensor,
+    dlog_probs: torch.FloatTensor,
+    hidden_states: torch.FloatTensor,
+    vocab_weights: torch.FloatTensor,
+    input_ids: torch.LongTensor,
+    temperature: float = 1.0,
+) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+    logits = (hidden_states @ vocab_weights.t()) / temperature
+    orig_dtype = logits.dtype
+    logits = logits.to(torch.float32)
+
+    # Slower but more numerically stable to do log_softmax, rather than probs.log()
+    probs = logits.softmax(dim=-1)
+    log_probs = logits.log_softmax(dim=-1)
+
+    # Gradient from entropy
+    entropy = torch.logsumexp(logits, dim=-1) - torch.sum(probs * logits, dim=-1)
+    dlogits = probs * (log_probs + entropy.unsqueeze(-1)) * (-dentropy.unsqueeze(-1))
+
+    # Gradient from log_probs
+    one_hot_input = torch.zeros_like(logits).scatter_(-1, input_ids.unsqueeze(-1), 1)
+    dlogits += dlog_probs.to(torch.float32).unsqueeze(-1) * (one_hot_input - probs)
+
+    dlogits = dlogits.to(orig_dtype) / temperature
+    dhidden_states = dlogits @ vocab_weights
+    dvocab_weights = (dlogits.transpose(-1, -2) @ hidden_states).sum(0)
+
+    return dhidden_states, dvocab_weights
+
+
+class FusedEntropyLogProbs(torch.autograd.Function):
+
+    @staticmethod
+    def forward(
+        ctx,
+        hidden_states: torch.FloatTensor,
+        vocab_weights: torch.FloatTensor,
+        input_ids: torch.LongTensor,
+        temperature: float = 1.0,
+        chunk_size: int = 512,
+    ) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+        # Cast to a 2D tensor of the shape [T, D] for ease of working
+        orig_ndim = hidden_states.ndim
+        assert orig_ndim in (2, 3), f"Invalid hidden_states shape, received {hidden_states.shape}"
+
+        if orig_ndim == 3:
+            assert input_ids.ndim == 2, f"input_ids shape doesn't match, {hidden_states.shape} {input_ids.shape}"
+            orig_batch_size = hidden_states.shape[0]
+            hidden_states = hidden_states.flatten(0, 1)
+            input_ids = input_ids.flatten(0, 1)
+
+        T = hidden_states.shape[0]
+
+        # Allocate memory for outputs
+        entropy = torch.empty(T, dtype=hidden_states.dtype, device=hidden_states.device)
+        log_probs = torch.empty(T, dtype=hidden_states.dtype, device=hidden_states.device)
+
+        # Perform forward one chunk at a time
+        for chunk_start in range(0, T, chunk_size):
+            chunk_end = min(chunk_start + chunk_size, T)
+
+            chunk_entropy, chunk_log_probs = fused_entropy_log_probs_fwd(
+                hidden_states=hidden_states[chunk_start:chunk_end],
+                vocab_weights=vocab_weights,
+                input_ids=input_ids[chunk_start:chunk_end],
+                temperature=temperature,
+            )
+            entropy[chunk_start:chunk_end] = chunk_entropy
+            log_probs[chunk_start:chunk_end] = chunk_log_probs
+
+        # Cast the output back to the original input dimension
+        if orig_ndim == 3:
+            entropy = entropy.view(orig_batch_size, -1)
+            log_probs = log_probs.view(orig_batch_size, -1)
+
+        ctx.save_for_backward(hidden_states, vocab_weights, input_ids)
+        ctx.temperature = temperature
+        ctx.chunk_size = chunk_size
+
+        return entropy, log_probs
+
+    @staticmethod
+    def backward(ctx, dentropy: torch.FloatTensor, dlog_probs: torch.FloatTensor):
+        hidden_states, vocab_weights, input_ids = ctx.saved_tensors
+        temperature = ctx.temperature
+        chunk_size = ctx.chunk_size
+
+        # Here orig_ndim refers to the orig_ndim of hidden_states
+        orig_ndim = dentropy.ndim + 1
+        if orig_ndim == 3:
+            orig_batch_size = dentropy.shape[0]
+            dentropy = dentropy.flatten()
+            dlog_probs = dlog_probs.flatten()
+
+        T = hidden_states.shape[0]
+
+        # Allocate memory for outputs
+        dhidden_states = torch.zeros_like(hidden_states)
+        dvocab_weights = torch.zeros_like(vocab_weights)
+
+        # Perform backward one chunk at a time
+        for chunk_start in range(0, T, chunk_size):
+            chunk_end = min(chunk_start + chunk_size, T)
+
+            h, v = fused_entropy_log_probs_bwd(
+                dentropy=dentropy[chunk_start:chunk_end],
+                dlog_probs=dlog_probs[chunk_start:chunk_end],
+                hidden_states=hidden_states[chunk_start:chunk_end],
+                vocab_weights=vocab_weights,
+                input_ids=input_ids[chunk_start:chunk_end],
+                temperature=temperature,
+            )
+
+            dhidden_states[chunk_start:chunk_end] += h
+            dvocab_weights += v
+
+        # Cast the output back to the original input dimension
+        if orig_ndim == 3:
+            hidden_size = hidden_states.shape[-1]
+            dhidden_states = dhidden_states.view(orig_batch_size, -1, hidden_size)
+
+        return (
+            dhidden_states,  # hidden_states
+            dvocab_weights,  # vocab_weights
+            None,  # input_ids
+            None,  # temperature
+            None,  # chunk_size
+        )
+
+
+fused_entropy_log_probs = FusedEntropyLogProbs.apply
