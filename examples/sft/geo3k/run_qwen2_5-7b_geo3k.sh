8d7c80bc0d44c4ee8e3cd11ce7e930c082a702cb
Rishu <rishu@orby.ai>
2025-05-21 12:08:19 -0700

    changes to support sft

--- examples/sft/geo3k/run_qwen2_5-7b_geo3k.sh
+++ examples/sft/geo3k/run_qwen2_5-7b_geo3k.sh
@@ -4,7 +4,7 @@
 set -x
 
 if [ "$#" -lt 2 ]; then
-    echo "Usage: run_qwen2_5_vl_7b_sft.sh <nproc_per_node> <save_path> [other_configs...]"
+    echo "Usage: run_qwen2_5-7b_geo3k.sh <nproc_per_node> <save_path> [other_configs...]"
     exit 1
 fi
 
@@ -14,34 +14,33 @@ save_path=$2
 # Shift the arguments so $@ refers to the rest
 shift 2
 
-# Export WANDB API key if needed
-# export WANDB_API_KEY=your_api_key_here
-
-torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
-     -m verl.trainer.fsdp_sft_trainer \
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=sft \
     data.train_files=$HOME/data/geo3k/train.parquet \
     data.val_files=$HOME/data/geo3k/test.parquet \
-    data.prompt_key=prompt \
-    data.response_key=reward_model \
-    data.response_dict_keys=['ground_truth'] \
-    data.image_key=images \
+    data.train_batch_size=16 \
     data.max_prompt_length=1024 \
     data.max_response_length=2048 \
     data.filter_overlong_prompts=True \
-    data.truncation='error' \
-    optim.lr=1e-6 \
-    data.micro_batch_size_per_gpu=2 \
-    model.partial_pretrain=Qwen/Qwen2.5-VL-7B-Instruct \
-    model.enable_gradient_checkpointing=True \
-    model.use_remove_padding=True \
+    data.truncation=error \
+    data.image_key=images \
+    actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-7B-Instruct \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.actor.lora_rank=32 \
+    actor_rollout_ref.actor.lora_alpha=16 \
+    actor_rollout_ref.actor.lora_target_modules=all-linear \
     trainer.default_local_dir=$save_path \
     trainer.project_name=geo3k-sft \
     trainer.experiment_name=geo3k-sft-qwen2.5-vl-7b-instruct \
     trainer.logger=['console','wandb'] \
-    trainer.total_epochs=3 \
+    trainer.n_gpus_per_node=$nproc_per_node \
+    trainer.nnodes=1 \
     trainer.save_freq=1 \
     trainer.test_freq=1 \
-    trainer.default_hdfs_dir=null \
-    model.lora_rank=32 \
-    model.lora_alpha=16 \
-    model.target_modules=all-linear $@
+    trainer.total_epochs=3 $@
