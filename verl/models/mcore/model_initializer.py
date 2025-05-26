3b82afc9ddd31ceb656b82068e3fb16a1cb1c102
Yan Bai <bayan@nvidia.com>
2025-05-25 19:52:14 -0700

    update deeepseekv3 initializer

--- verl/models/mcore/model_initializer.py
+++ verl/models/mcore/model_initializer.py
@@ -171,36 +171,15 @@ class DeepseekV3Model(BaseModelInitializer):
         share_embeddings_and_output_weights=False,
         value=False,
         freeze_moe_router=True,
-        **extra_kwargs,
+        **kwargs,
     ):
-        tfconfig = self.tfconfig
-        hf_config = self.hf_config
-        use_te = True
         if freeze_moe_router:
             self.tfconfig.moe_router_load_balancing_type = "none"
-
-        assert self.tfconfig.normalization == "RMSNorm", "only RMSNorm is supported for now"
-        transformer_layer_spec = get_gpt_decoder_block_spec(tfconfig, use_transformer_engine=use_te)
-        model = GPTModel(
-            config=tfconfig,
-            transformer_layer_spec=transformer_layer_spec,
-            vocab_size=hf_config.vocab_size,
-            max_sequence_length=hf_config.max_position_embeddings,
-            pre_process=pre_process,
-            post_process=post_process,
-            share_embeddings_and_output_weights=share_embeddings_and_output_weights,
-            position_embedding_type="rope",
-            rotary_base=hf_config.rope_theta,
-        )
+        model = super().initialize(**kwargs)
         if freeze_moe_router:
             for layer in model.decoder.layers:
                 if hasattr(layer.mlp, "router"):
                     layer.mlp.router.weight.requires_grad = False
-
-        if post_process and value:
-            from verl.models.llama.megatron.layers.parallel_linear import LinearForLastLayer
-
-            model.output_layer = LinearForLastLayer(input_size=tfconfig.hidden_size, output_size=1, config=tfconfig)
         return model
 
 
