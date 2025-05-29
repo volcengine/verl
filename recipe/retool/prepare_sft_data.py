40e62d829d8ce99e7d4803cf6c60314151f6f767
zhangchi.usc1992 <zhangchi.usc1992@bytedance.com>
2025-05-29 14:39:38 +0800

    update prepare_sft_data

--- recipe/retool/prepare_sft_data.py
+++ recipe/retool/prepare_sft_data.py
@@ -5,18 +5,6 @@ from verl.utils.reward_score.math import last_boxed_only_string, remove_boxed
 
 sft_dataset = datasets.load_dataset('JoeYing/ReTool-SFT')['train']
 
-ci_user_prompt_template_v3 = '''Solve the following problem step by step. You now have the ability to selectively write executable Python code to enhance your reasoning process. The Python code will be executed by an external sandbox, and the output (wrapped in `<interpreter>output_str</interpreter>`) can be returned to aid your reasoning and help you arrive at the final answer. The Python code should be complete scripts, including necessary imports. 
-Each code snippet is wrapped with `<code>\n```python\ncode snippet\n```\n</code>`.
-The last part of your response should be in the following format:
-<answer>\n\\boxed{{'The final answer goes here.'}}\n</answer>
-
-*user question:*
-{question}
-
-Remember to place the final answer in the last part using the format: 
-<answer>\n\\boxed{{'The final answer goes here.'}}\n</answer>'''
-
-
 from transformers.utils import get_json_schema
 
 import sys
@@ -141,11 +129,14 @@ def make_map_fn(split):
             new_messages = []
 
         return {
-            'messages': new_messages
+            'messages': new_messages,
+            'tools': TOOLS,
         }
-       
+
     return process_fn
 
 
 sft_dataset = sft_dataset.map(function=make_map_fn("train"), with_indices=True)
 sft_dataset = sft_dataset.filter(lambda x: len(x['messages']) > 0)
+
+sft_dataset.to_parquet('/mnt/hdfs/zhangchi.usc1992_ssd_hldy/public_exp/data/retool_sft_dataset.parquet')
