a0ba37cf8250745a2842112e96597b87521b71a3
LingChang <changling@mail.ustc.edu.cn>
2025-05-29 10:16:15 +0000

    Add license notice for Search-R1 adapted code

--- verl/utils/reward_score/search_r1_like_qa_em.py
+++ verl/utils/reward_score/search_r1_like_qa_em.py
@@ -1,5 +1,6 @@
 # Copyright 2024 Bytedance Ltd. and/or its affiliates
 # Copyright 2023-2024 SGLang Team
+# Copyright 2025 Search-R1 Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Adapted from https://github.com/PeterGriffinJin/Search-R1/blob/main/verl/utils/reward_score/qa_em.py
 
 import random
 import re
