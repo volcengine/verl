d56271d6004102da57108c247bee648fb71bd46e
Haibin Lin <haibin.lin@bytedance.com>
2025-05-30 15:05:27 -0700

    use myst

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = "index"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "recommonmark",
+    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.autosectionlabel",
