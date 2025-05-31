4ffe7059718552d035d5a834240796e368311f40
Yan Bai <bayan@nvidia.com>
2025-05-30 20:36:15 -0700

    add copyright

--- verl/models/mcore/qwen2_5_vl/vision_config.py
+++ verl/models/mcore/qwen2_5_vl/vision_config.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2024 Alibaba PAI and Nvidia Megatron-LM Team.
+# Copyright 2025 Bytedance Ltd. and/or its affiliates
+# Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2024 Alibaba PAI Team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
