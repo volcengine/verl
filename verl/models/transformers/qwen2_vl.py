1a3a5151b101dc40d6c3d26ba6cab3897b1bb590
gaoziyuan.955 <gaoziyuan.955@bytedance.com>
2025-06-03 16:51:57 +0800

    error args

--- verl/models/transformers/qwen2_vl.py
+++ verl/models/transformers/qwen2_vl.py
@@ -413,7 +413,6 @@ def forward_with_torch_backend(
         position_ids=position_ids,
         past_key_values=past_key_values,
         inputs_embeds=inputs_embeds,
-        labels=labels,
         use_cache=use_cache,
         output_attentions=output_attentions,
         output_hidden_states=output_hidden_states,
