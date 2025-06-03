559e1b88287a3bfd5d858c5dfa6411798eb63a6d
gaoziyuan.955 <gaoziyuan.955@bytedance.com>
2025-06-03 16:49:47 +0800

    fix exp name:

--- examples/ppo_trainer/run_qwen2-7b_rm_seq_balance_fused_kernels.sh
+++ examples/ppo_trainer/run_qwen2-7b_rm_seq_balance_fused_kernels.sh
@@ -55,7 +55,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='verl_example_gsm8k' \
-    trainer.experiment_name='qwen2-7b_hybrid_rm_bsz8k_p4k_r4k_seq_packing' \
+    trainer.experiment_name='qwen2-7b_hybrid_rm_bsz8k_p4k_r4k_seq_packing_fused_kernel' \
     trainer.n_gpus_per_node=8 \
     trainer.val_before_train=False \
     trainer.nnodes=1 \
