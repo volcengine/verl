dc3157e04e47da6f9c1b011a2f5341db3e4fb26e
Xiang Long <mindsculptor@yeah.net>
2025-06-05 01:03:04 +0800

    Lower gpu mem foot print

--- examples/sglang_multiturn/run_qwen2.5-3b_gsm8k_multiturn_w_interaction_mb.sh
+++ examples/sglang_multiturn/run_qwen2.5-3b_gsm8k_multiturn_w_interaction_mb.sh
@@ -48,10 +48,11 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.actor.fsdp_config.param_offload=$OFFLOAD \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=$OFFLOAD \
+    +actor_rollout_ref.actor.fsdp_config.model_dtype=bfloat16 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=$MICRO_BATCH_SIZE \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=sglang \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
     actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=$MICRO_BATCH_SIZE \
     actor_rollout_ref.ref.fsdp_config.param_offload=$OFFLOAD \
