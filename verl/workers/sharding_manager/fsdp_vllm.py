3e9f2bc1e281121ddad582e1f9ce26b30f1e1385
ETOgaosion <gaoziyuan19@mails.ucas.ac.cn>
2025-06-06 01:32:01 +0800

    fix fsdp vllm import

--- verl/workers/sharding_manager/fsdp_vllm.py
+++ verl/workers/sharding_manager/fsdp_vllm.py
@@ -36,9 +36,9 @@ from verl.protocol import all_gather_data_proto
 from verl.third_party.vllm import LLM, vllm_version
 from verl.third_party.vllm import parallel_state as vllm_ps
 from verl.utils.debug import GPUMemoryLogger, log_gpu_memory_usage
+from verl.utils.debug.performance import _timer
 from verl.utils.device import get_torch_device
 from verl.utils.fsdp_utils import fsdp_version, layered_summon_lora_params, load_fsdp_model_to_gpu, offload_fsdp_model_to_cpu
-from verl.utils.timing_utils import _timer
 from verl.utils.torch_functional import check_cuda_is_available
 from verl.utils.vllm_utils import TensorLoRARequest, VLLMHijack, is_version_ge, patch_vllm_moe_model_weight_loader
 
