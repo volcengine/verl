0c4338f02b89aa7e96e593c2f6931f5af778f49d
Xiang Long <mindsculptor@yeah.net>
2025-06-06 11:11:28 +0800

    Fix prompt in preprocess interaction

--- examples/data_preprocess/gsm8k_multiturn_w_interaction.py
+++ examples/data_preprocess/gsm8k_multiturn_w_interaction.py
@@ -65,9 +65,7 @@ if __name__ == "__main__":
                         "role": "system",
                         "content": (
                             "You are a math expert. You are given a question and you need to solve it step by step. "
-                            "Reasoning step by step before any tool call. "
-                            "You should use the `calc_gsm8k_reward` tool after step by step solving the question, "
-                            "before generate final answer at least once and refine your answer if necessary. "
+                            "You should rethinking carefully if user point out your answer is wrong. "
                             "Put your final answer in the format of `#### <answer>`."
                         ),
                     },
