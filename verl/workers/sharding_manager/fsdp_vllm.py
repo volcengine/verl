96dcf92de9bab9e833ed243148bb82ab50777a8a
hiyouga <hiyouga@buaa.edu.cn>
2025-06-06 06:59:36 +0000

    fix qwen2vl train with transformers latest

--- verl/workers/sharding_manager/fsdp_vllm.py
+++ verl/workers/sharding_manager/fsdp_vllm.py
@@ -38,6 +38,7 @@ from verl.third_party.vllm import parallel_state as vllm_ps
 from verl.utils.debug import GPUMemoryLogger, log_gpu_memory_usage
 from verl.utils.device import get_torch_device
 from verl.utils.fsdp_utils import fsdp_version, layered_summon_lora_params, load_fsdp_model_to_gpu, offload_fsdp_model_to_cpu
+from verl.utils.model import convert_weight_keys
 from verl.utils.torch_functional import check_cuda_is_available
 from verl.utils.vllm_utils import TensorLoRARequest, VLLMHijack, is_version_ge, patch_vllm_moe_model_weight_loader
 
@@ -164,6 +165,8 @@ class FSDPVLLMShardingManager(BaseShardingManager):
             params = __collect_lora_params()
         else:
             params = self.module.state_dict()
+
+        params = convert_weight_keys(params, getattr(self.module, "_fsdp_wrapped_module", self.module))
         log_gpu_memory_usage("After state_dict() in sharding manager memory", logger=logger)
 
         # Copy, not share memory
