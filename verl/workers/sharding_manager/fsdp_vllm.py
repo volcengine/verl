e8bf5fcafcac975c9e165aaeaa5de1cc714a072d
qichang.dong <dongqichang@ecmas.ai>
2025-06-08 07:11:58 +0800

    fix error in __collect_lora_params() in FSDPVLLMShardingManager

--- verl/workers/sharding_manager/fsdp_vllm.py
+++ verl/workers/sharding_manager/fsdp_vllm.py
@@ -135,7 +135,7 @@ class FSDPVLLMShardingManager(BaseShardingManager):
                     lora_params = get_peft_model_state_dict(peft_model)
                 else:
                     model = peft_model.base_model.model
-                    orig_dev = "cpu" if "cpu" in next(model.parameters()).device else "cuda"
+                    orig_dev = "cpu" if "cpu" in str(next(model.parameters()).device) else "cuda"
                     model = model.to("cpu")
                     for name, param in model.state_dict().items():
                         if any(x in name for x in ["_flat_param", "lora_"]):
