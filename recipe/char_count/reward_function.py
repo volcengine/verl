95333a0d90bead0821e9b85449dcfbbf729349ba
Chi Zhang <czhangseu@gmail.com>
2025-06-08 09:19:57 +0800

    add license

--- recipe/char_count/reward_function.py
+++ recipe/char_count/reward_function.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Reward function
 """
@@ -15,6 +29,6 @@ def char_count_reward_function(data_source, solution_str, ground_truth, extra_in
             return 1
         else:
             return 0
-    except:
+    except Exception:
         print(ground_truth, solution_str)
         return 0
