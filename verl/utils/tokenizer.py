864eaf69746653bee056e8f4b247203c35fd04c8
Haibin Lin <haibin.lin@bytedance.com>
2025-06-11 21:20:06 -0700

    fix

--- verl/utils/tokenizer.py
+++ verl/utils/tokenizer.py
@@ -76,6 +76,8 @@ def hf_processor(name_or_path, **kwargs):
         processor = AutoProcessor.from_pretrained(name_or_path, **kwargs)
     except Exception as e:
         processor = None
+        # TODO(haibin.lin): try-catch should be removed after adding transformer version req to setup.py to avoid
+        # silent failure
         warnings.warn(f"Failed to create processor: {e}. This may affect multimodal processing")
     # Avoid load tokenizer, see:
     # https://github.com/huggingface/transformers/blob/v4.49.0/src/transformers/models/auto/processing_auto.py#L344
