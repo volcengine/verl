eb5e029807a655bbdd23621baed32ee33560730f
Ethen Liu <ethen8181@gmail.com>
2025-06-17 15:27:22 -0700

    feat: add comment and unit-test

---
+++ tests/workers/test_fsdp_workers.py
@@ -0,0 +1,45 @@
+import os
+from omegaconf import OmegaConf
+from verl.workers.fsdp_workers import ActorRolloutRefWorker
+
+
+def test_actor_rollout_ref_worker_actor_ref_model():
+    """Test specifying different reference/actor model"""
+    os.environ["RANK"] = "0"
+    os.environ["WORLD_SIZE"] = "1"
+    os.environ["MASTER_ADDR"] = "127.0.0.1"
+    os.environ["MASTER_PORT"] = "8888"
+
+    config_str = """
+    model:
+      path: Qwen/Qwen2.5-1.5B-Instruct
+    actor:
+      strategy: fsdp
+      fsdp_config:
+        fsdp_size: -1
+        forward_prefetch: false
+    ref:
+      model:
+        path: Qwen/Qwen2.5-7B-Instruct
+      fsdp_config:
+        fsdp_size: -1
+      log_prob_micro_batch_size: 1
+      ulysses_sequence_parallel_size: 1
+      entropy_from_logits_with_chunking: false
+    """
+    dict_conf = OmegaConf.create(config_str)
+    actor_rollout_ref_worker = ActorRolloutRefWorker(dict_conf, role="ref")
+    actor_rollout_ref_worker.init_model()
+
+    model_config = actor_rollout_ref_worker.ref_module_fsdp._fsdp_wrapped_module.config
+    assert model_config.hidden_size == 3584
+
+    # set ref.model to null, fallback to default case where actor is the same as reference
+    dict_conf["ref"]["model"] = None
+    actor_rollout_ref_worker = ActorRolloutRefWorker(dict_conf, role="ref")
+    actor_rollout_ref_worker.init_model()
+
+    model_config = actor_rollout_ref_worker.ref_module_fsdp._fsdp_wrapped_module.config
+    assert model_config.hidden_size == 1536
+
+
