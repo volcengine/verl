781510d7fa9fa9b32689069bfd50c659ce0dc5d9
yuyangding <yuyangding@bytedance.com>
2025-06-23 03:07:47 +0800

    modify async to multiprocess

--- recipe/api-genrm/reward_function.py
+++ recipe/api-genrm/reward_function.py
@@ -11,9 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import asyncio
 
-from openai import AsyncOpenAI
+from time import sleep
+
+import openai
+from openai import OpenAI
+from concurrent.futures import ThreadPoolExecutor
 
 from verl.utils.reward_score.math import last_boxed_only_string, remove_boxed
 
@@ -35,34 +38,49 @@ Please put your final answer (i.e., 'True' or 'False') in \\boxed{{}}.
 
 BASE_URL = "http://localhost:8000/v1"
 API_KEY = "EMPTY"
-MAX_RETRIES = 3
+MAX_RETRIES = 1
 BASE_DELAY = 2
+MAX_WORKERS = 64
 
 
-async def get_response(client, problem, solution_str):
+def get_response(client, problem, solution_str):
     prompt = GENRM_PROMPT_TEMPLATE.format(problem=problem, solution=solution_str)
     messages = [{"role": "user", "content": prompt}]
     for attempt in range(MAX_RETRIES):
         try:
-            output = await client.chat.completions.create(
+            output = client.chat.completions.create(
                 model="genrm-demo",
                 messages=messages,
                 max_tokens=4096,
                 temperature=0.0,
             )
             return output.choices[0].message.content
-        except Exception as e:
+        except (
+            openai.APIError,
+            openai.RateLimitError,
+            openai.InternalServerError,
+            openai.OpenAIError,
+            openai.APIStatusError,
+            openai.APITimeoutError,
+            openai.InternalServerError,
+            openai.APIConnectionError,
+        ) as e:
             if attempt < MAX_RETRIES - 1:
+                print("Exception: ", repr(e))
                 delay = BASE_DELAY * (2**attempt)
-                print(f"Connection error: {e}. Retrying in {delay} seconds...")
-                await asyncio.sleep(delay)
+                print(f"Retrying in {delay} seconds...")
+                sleep(delay)
             else:
                 print(f"Failed after {MAX_RETRIES} attempts. Error: {e}")
+        except Exception as e:
+            print(f"Failed to run the model for {prompt}!")
+            print("Exception: ", repr(e))
+            raise e
     return None
 
 
 def compute_reward(response):
-    reward_score = 0.0
+    reward_score = 0.5
     try:
         boxed_result = last_boxed_only_string(response)
         if boxed_result is not None:
@@ -73,36 +91,44 @@ def compute_reward(response):
     return reward_score
 
 
-async def compute_score(data_source, solution_str, ground_truth, extra_info=None):
+def compute_score(data_source, solution_str, ground_truth, extra_info):
     split = extra_info["split"]
-    from verl.utils.reward_score.gsm8k import compute_score
+    from verl.utils.reward_score.math import compute_score
 
     func_rm_score = compute_score(solution_str, ground_truth)
+
     if split == "test":
         return func_rm_score
     else:  # split = "train"
         if func_rm_score == 0.0:
             return func_rm_score
 
-        client = AsyncOpenAI(
+        client = OpenAI(
             base_url=BASE_URL,
             api_key=API_KEY,
             timeout=90,
         )
         problem = extra_info["question"]
-        response = await get_response(client, problem, solution_str)
+        response = get_response(client, problem, solution_str)
         if response is not None:
             reward_score = compute_reward(response)
         else:
-            reward_score = 0.0
+            reward_score = 0.5
 
         return reward_score
 
 
-def compute_score_batch(data_sources, solution_strs, ground_truths, extra_infos=None):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+def compute_score_batch(data_sources, solution_strs, ground_truths, extra_infos):
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        futures = []
+        for data_source, solution_str, ground_truth, extra_info in zip(
+            data_sources, solution_strs, ground_truths, extra_infos
+        ):
+            future = executor.submit(
+                compute_score, data_source, solution_str, ground_truth, extra_info
+            )
+            futures.append(future)
+        
+        results = [future.result() for future in futures]
 
-    tasks = [compute_score(data_source, solution_str, ground_truth, extra_info) for data_source, solution_str, ground_truth, extra_info in zip(data_sources, solution_strs, ground_truths, extra_infos)]
-    results = loop.run_until_complete(asyncio.gather(*tasks))
     return results
