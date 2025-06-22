98ac3f12da49508b4b1a11907253beea17b0a533
nanjiangwill <willjiang2018@gmail.com>
2025-06-21 21:02:28 -0700

    lint

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -885,6 +885,12 @@ class CriticWorker(Worker, DistProfilerExtension):
         self.tokenizer = hf_tokenizer(tokenizer_path, trust_remote_code=config.model.get("trust_remote_code", False))
         self.processor = hf_processor(tokenizer_path, trust_remote_code=config.model.get("trust_remote_code", False))
 
+        if self.config.model.get("custom_chat_template", None) is not None:
+            if self.processor is not None:
+                self.processor.chat_template = self.config.model.custom_chat_template
+            else:
+                self.tokenizer.chat_template = self.config.model.custom_chat_template
+
         override_config = OmegaConf.to_container(self.config.model.get("override_config", OmegaConf.create()))
         override_config_kwargs = {
             "bos_token_id": self.tokenizer.bos_token_id,
