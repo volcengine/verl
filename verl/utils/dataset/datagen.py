9c651682daa9b939b2fb43114365a245864b45fc
Justin Wong <wong.justin@berkeley.edu>
2025-07-01 17:42:18 +0000

    update license

--- verl/utils/dataset/datagen.py
+++ verl/utils/dataset/datagen.py
@@ -11,6 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+FSDP PPO Trainer with Ray-based single controller.
+This trainer supports model-agonistic model initialization with huggingface
+"""
+
 from abc import ABC, abstractmethod
 
 from omegaconf import DictConfig
