b18e53770d7f0e198ba2785679520f4cee59968d
tongtong0613 <614452178@qq.com>
2025-07-04 11:06:20 +0800

    typo fix

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -75,7 +75,11 @@ def run_ppo(config) -> None:
 
     # Create a remote instance of the TaskRunner class, and
     # Execute the `run` method of the TaskRunner instance remotely and wait for it to complete
-    if is_cuda_available and config.trainer.get("profile_steps") is not None and len(config.trainer.get("profile_steps", [])) > 0:
+    if (
+        is_cuda_available
+        and OmegaConf.select(config.trainer, "profile_steps") is not None
+        and len(OmegaConf.select(config.trainer, "profile_steps")) > 0
+    ):
         nsight_options = OmegaConf.to_container(config.trainer.controller_nsight_options)
         runner = TaskRunner.options(runtime_env={"nsight": nsight_options}).remote()
     else:
