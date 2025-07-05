a66046545f441b8e02f7b8cbab8e455eca626aa3
H <linhaibin.eric@gmail.com>
2025-07-04 18:52:51 -0700

    address comments

--- verl/workers/sharding_manager/base.py
+++ verl/workers/sharding_manager/base.py
@@ -19,9 +19,11 @@ from verl import DataProto
 
 
 class BaseShardingManager:
-    def __enter__(self):
+    def __init__(self):
         self.timing = {}
 
+    def __enter__(self):
+        pass
 
     def __exit__(self, exc_type, exc_value, traceback):
         pass
