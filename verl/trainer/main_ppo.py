24c3079da6ea4c2119ad8c1d081794c016bb78e2
H <linhaibin.eric@gmail.com>
2025-07-08 15:37:41 -0700

    Update main_ppo.py

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -300,7 +300,7 @@ def create_rl_sampler(data_config, dataset):
             data_config=data_config,
         )
         assert isinstance(sampler, AbstractSampler)
-        assert data_config.dataloader_num_workers == 0, (
+        assert data_config.get("dataloader_num_workers", 8) == 0, (
             "If using curriculum, num_workers must be 0 to prevent data caching. "
             "If the dataloader caches data before the batch is done the "
             "curriculum sampler won't have the opportunity to reorder it. "
