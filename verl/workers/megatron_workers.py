f1c3f1db82e7f1a0b3883691096790a93c24c491
Bassil Shama <bassil.shama@gmail.com>
2025-07-10 17:25:20 +0000

    cleanup

--- verl/workers/megatron_workers.py
+++ verl/workers/megatron_workers.py
@@ -647,7 +647,11 @@ class ActorRolloutRefWorker(MegatronWorker, DistProfilerExtension):
         if self._is_offload_param:
             load_megatron_model_to_gpu(self.actor_module)
         self.checkpoint_mananager.save_checkpoint(
-            local_path=checkpoint_path, hdfs_path=hdfs_path, global_step=global_step, max_ckpt_to_keep=max_ckpt_to_keep
+            local_path=checkpoint_path,
+            hdfs_path=hdfs_path,
+            remote_path=remote_path,
+            global_step=global_step,
+            max_ckpt_to_keep=max_ckpt_to_keep,
         )
         torch.distributed.barrier()
         if self._is_offload_param:
@@ -979,7 +983,11 @@ class CriticWorker(MegatronWorker, DistProfilerExtension):
         if self._is_offload_param:
             load_megatron_model_to_gpu(self.critic_module)
         self.checkpoint_mananager.save_checkpoint(
-            local_path=checkpoint_path, hdfs_path=hdfs_path, global_step=global_steps, max_ckpt_to_keep=max_ckpt_to_keep
+            local_path=checkpoint_path,
+            hdfs_path=hdfs_path,
+            remote_path=remote_path,
+            global_step=global_steps,
+            max_ckpt_to_keep=max_ckpt_to_keep,
         )
         if self._is_offload_param:
             offload_megatron_model_to_cpu(self.critic_module)
