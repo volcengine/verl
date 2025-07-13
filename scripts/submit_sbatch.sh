226d704260640c0df889dcf52cab83c89d5af96e
Sam Shleifer <sshleifer@gmail.com>
2025-07-13 16:48:15 +0000

    wip

--- scripts/submit_sbatch.sh
+++ scripts/submit_sbatch.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name=verl-ray-on-slurm
-#SBATCH --nodes=4
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-node=2
 #SBATCH --mem=200G
-#SBATCH --time=3:00:00
 #SBATCH --gpus-per-node=8
 #SBATCH --cpus-per-task=64
 #SBATCH --output=/home/sam/verl_log/slurm-%j.out
@@ -35,6 +34,7 @@ export DATA_PATH="$HOME/data"
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export VLLM_USE_V1=0  # Disable V1: not compatible with multinode training
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # requeue on sigusr1
 # Function to handle the SIGUSR1 signal
 # handle_sigusr1() {
@@ -138,11 +138,7 @@ echo "=== Ray test completed ==="
 
 echo "Start to train..."
 
-# Fix flash_attn compatibility issue by removing it completely
-echo "Removing flash_attn to avoid compatibility issues..."
-#bash -c "eval \"\$(/home/sam/miniconda3/bin/conda shell.bash hook)\"; conda activate verl; pip uninstall flash_attn -y"
-
-MODEL_PATH="Qwen/Qwen2-7B-Instruct"
+MODEL_PATH="Qwen/Qwen2.5-3B-Instruct"
 
 
 PYTHONUNBUFFERED=1 srun --overlap --nodes=${SLURM_NNODES} --ntasks=1 -w "$head_node" \
@@ -151,29 +147,31 @@ PYTHONUNBUFFERED=1 srun --overlap --nodes=${SLURM_NNODES} --ntasks=1 -w "$head_n
     algorithm.adv_estimator=grpo \
     data.train_files=$DATA_PATH/polaris/train.parquet \
     data.val_files=$DATA_PATH/polaris/test.parquet \
-    data.train_batch_size=8 \
+    data.train_batch_size=256 \
     data.max_prompt_length=2048 \
-    data.max_response_length=8192 \
+    data.max_response_length=4096 \
     data.filter_overlong_prompts=True \
-    data.truncation='error' \
+    data.truncation=error \
     actor_rollout_ref.model.path=$MODEL_PATH \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.actor.strategy=fsdp2 \
-    actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+    actor_rollout_ref.model.enable_gradient_checkpointing=False \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
     actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.free_cache_engine=True \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.ref.strategy=fsdp2 \
@@ -181,13 +179,15 @@ PYTHONUNBUFFERED=1 srun --overlap --nodes=${SLURM_NNODES} --ntasks=1 -w "$head_n
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger="['console','wandb']" \
-    trainer.project_name='ssverl' \
-    trainer.experiment_name='qwen2_7b.multinode' \
+    trainer.project_name=ssverl \
+    trainer.experiment_name=qwen2_3b.multinode \
     critic.model.fsdp_config.param_offload=True \
     critic.model.fsdp_config.optimizer_offload=True \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=${SLURM_NNODES} \
-    trainer.save_freq=10 \
+    trainer.save_freq=50 \
+    trainer.max_actor_ckpt_to_keep=3 \
+    trainer.max_critic_ckpt_to_keep=3 \
     trainer.test_freq=3 \
     trainer.total_epochs=20 \
     trainer.val_before_train=True"
