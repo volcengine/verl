9a9b9aa0bb4469cbc661cf3428f85e72d94d6fdf
H <linhaibin.eric@gmail.com>
2025-07-14 19:01:55 -0700

    Update ray_trainer.py

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-FSDP PPO Trainer with Ray-based single controller.
+PPO Trainer with Ray-based single controller.
 This trainer supports model-agonistic model initialization with huggingface
 """
 
