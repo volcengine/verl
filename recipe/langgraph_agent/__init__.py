49bd976fe93c95a72362cd221e546c1b6623bfa8
wuxibin <wuxibin@bytedance.com>
2025-07-15 11:11:07 +0800

    remove dead code

--- recipe/langgraph_agent/__init__.py
+++ recipe/langgraph_agent/__init__.py
@@ -11,9 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from recipe.langgraph_agent.react_agent_loop import ReactAgentLoop
-from verl.experimental.agent_loop.agent_loop import AgentLoopBase
-
-
-def agent_loop_loader() -> dict[str, AgentLoopBase]:
-    return {"react_agent": ReactAgentLoop}
