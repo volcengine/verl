58d757ac8e022470141cbbaa23ba12221e6cac78
Lin Yuan <apeforest@gmail.com>
2025-07-18 10:19:20 -0700

    fix lint

--- examples/sft/gsm8k/run_gemma_7b.sh
+++ examples/sft/gsm8k/run_gemma_7b.sh
@@ -18,7 +18,7 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
     data.prompt_key=extra_info \
     data.response_key=extra_info \
     data.prompt_dict_keys=['question'] \
-    +data.response_dict_keys=['answer'] \
+    data.response_dict_keys=['answer'] \
     data.micro_batch_size_per_gpu=4 \
     model.partial_pretrain=google/gemma-1.1-7b-it \
     trainer.default_local_dir=$save_path \
