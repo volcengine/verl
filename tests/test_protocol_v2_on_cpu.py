28daa7cc16b89ad34d28daa204697de549f509f5
Chi Zhang <czhangseu@gmail.com>
2025-07-21 23:41:59 +0800

    update

--- tests/test_protocol_v2_on_cpu.py
+++ tests/test_protocol_v2_on_cpu.py
@@ -86,7 +86,7 @@ def test_tensor_dict_constructor():
     assert data[0:2]["data_source"] == ["gsm8k"] * 2
 
     # test non tensor data
-    data["name"] == "abdce"
+    assert data["name"] == "abdce"
 
 def test_tensordict_with_images():
     # each sample contains a sequence with multiple images of different sizes
@@ -220,7 +220,12 @@ def test_reorder():
 def test_chunk_concat():
     obs = torch.tensor([1, 2, 3, 4, 5, 6])
     labels = ["a", "b", "c", "d", "e", "f"]
-    data = DataProto.from_dict(tensors={"obs": obs}, non_tensors={"labels": labels}, meta_info={"name": "abdce"})
+    data = tu.get_tensordict({"obs": obs, "labels": labels}, non_tensor_dict={"name": "abcde"})
+
+    from IPython import embed
+    embed()
+
+    # note that chunk doesn't guarantee the number of output chunks
 
     with pytest.raises(AssertionError):
         data.chunk(5)
@@ -244,39 +249,40 @@ def test_chunk_concat():
 def test_pop():
     obs = torch.randn(100, 10)
     act = torch.randn(100, 3)
-    dataset = DataProto.from_dict({"obs": obs, "act": act}, meta_info={"2": 2, "1": 1})
-    poped_dataset = dataset.pop(batch_keys=["obs"], meta_info_keys=["2"])
+    dataset = tu.get_tensordict({"obs": obs, "act": act}, non_tensor_dict={"2": 2, "1": 1})
 
-    assert poped_dataset.batch.keys() == {"obs"}
-    assert poped_dataset.meta_info.keys() == {"2"}
+    poped_dataset = tu.pop(dataset, keys=["obs", "2"])
 
-    assert dataset.batch.keys() == {"act"}
-    assert dataset.meta_info.keys() == {"1"}
+    assert poped_dataset.batch_size[0] == 100
+
+    assert poped_dataset.keys() == {"obs", "2"}
+
+    assert dataset.keys() == {"act", "1"}
 
 
 def test_repeat():
     # Create a DataProto object with some batch and non-tensor data
     obs = torch.tensor([[1, 2], [3, 4], [5, 6]])
     labels = ["a", "b", "c"]
-    data = DataProto.from_dict(tensors={"obs": obs}, non_tensors={"labels": labels}, meta_info={"info": "test_info"})
+    data = tu.get_tensordict({"obs": obs, "labels": labels}, non_tensor_dict={"info": "test_info"})
 
     # Test interleave=True
-    repeated_data_interleave = data.repeat(repeat_times=2, interleave=True)
+    repeated_data_interleave = data.repeat_interleave(repeats=2)
     expected_obs_interleave = torch.tensor([[1, 2], [1, 2], [3, 4], [3, 4], [5, 6], [5, 6]])
     expected_labels_interleave = ["a", "a", "b", "b", "c", "c"]
 
-    assert torch.all(torch.eq(repeated_data_interleave.batch["obs"], expected_obs_interleave))
-    assert (repeated_data_interleave.non_tensor_batch["labels"] == expected_labels_interleave).all()
-    assert repeated_data_interleave.meta_info == {"info": "test_info"}
+    assert torch.all(torch.eq(repeated_data_interleave["obs"], expected_obs_interleave))
+    assert (repeated_data_interleave["labels"] == expected_labels_interleave)
+    assert repeated_data_interleave["info"] == "test_info"
 
     # Test interleave=False
-    repeated_data_no_interleave = data.repeat(repeat_times=2, interleave=False)
+    repeated_data_no_interleave = data.repeat(2)
     expected_obs_no_interleave = torch.tensor([[1, 2], [3, 4], [5, 6], [1, 2], [3, 4], [5, 6]])
     expected_labels_no_interleave = ["a", "b", "c", "a", "b", "c"]
 
-    assert torch.all(torch.eq(repeated_data_no_interleave.batch["obs"], expected_obs_no_interleave))
-    assert (repeated_data_no_interleave.non_tensor_batch["labels"] == expected_labels_no_interleave).all()
-    assert repeated_data_no_interleave.meta_info == {"info": "test_info"}
+    assert torch.all(torch.eq(repeated_data_no_interleave["obs"], expected_obs_no_interleave))
+    assert (repeated_data_no_interleave["labels"] == expected_labels_no_interleave)
+    assert repeated_data_no_interleave["info"] == "test_info"
 
 
 def test_dataproto_pad_unpad():
--- verl/utils/tensordict_utils.py
+++ verl/utils/tensordict_utils.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Iterator
+
 import torch
 from tensordict import TensorDict
 from tensordict.tensorclass import NonTensorData
@@ -128,4 +130,89 @@ def assert_tensordict_eq(tensordict1: TensorDict, tensordict2: TensorDict):
             assert torch.all(torch.eq(val, val2)).item()
         else:
             assert val == val2
+
+
+def pop(tensordict: TensorDict, keys: Iterator[str]) -> TensorDict:
+    tensor_output = {}
+    non_tensor_output = {}
+    for key in keys:
+        output = tensordict.get(key)
+        if isinstance(output, torch.Tensor | list):
+            tensor_output[key] = tensordict.pop(key)
+        else:
+            assert isinstance(output, NonTensorData)
+            non_tensor_output[key] = tensordict.pop(key)
+
+    return get_tensordict(tensor_output, non_tensor_output)
+
+#
+# def tensor_split(
+#     a: TensorLikeType,
+#     indices_or_sections: Union[Tensor, DimsType],
+#     dim: int = 0,
+# ) -> tuple[TensorLikeType, ...]:
+#     _dim = utils.canonicalize_dim(a.ndim, dim)
+#     if a.ndim == 0:
+#         msg = "tensor_split: received a rank zero tensor, but expected a tensor of rank one or greater!"
+#         raise ValueError(msg)
+#
+#     # If indices_or_sections is a tensor, it must be a CPU Long tensor
+#     if isinstance(indices_or_sections, TensorLike):
+#         if not indices_or_sections.device.type == "cpu":
+#             msg = (
+#                 f"tensor_split: if indices_or_sections is a tensor it must be on the CPU, "
+#                 f"but received one on {indices_or_sections.device}"
+#             )
+#             raise ValueError(msg)
+#         if indices_or_sections.dtype != torch.long:
+#             msg = (
+#                 "tensor_split: if indices_or_sections is a tensor it must have long dtype, "
+#                 f" but received one with dtype {indices_or_sections.dtype}"
+#             )
+#             raise ValueError(msg)
+#
+#     # Case 0 -- indices_or_sections is an integer or a scalar tensor n and a is split along dim into n parts of equal-ish length
+#     if isinstance(indices_or_sections, IntLike) or (
+#         isinstance(indices_or_sections, TensorLike) and indices_or_sections.ndim == 0
+#     ):
+#         sections: int = (
+#             indices_or_sections  # type: ignore[assignment]
+#             if isinstance(indices_or_sections, Number)
+#             else indices_or_sections.item()
+#         )
+#
+#         if sections <= 0:
+#             msg = f"tensor_split: number of sections must be greater than 0, but was {sections}"
+#             raise ValueError(msg)
+#
+#         dim_size = a.shape[_dim]
+#         min_split_size = math.floor(dim_size / sections)
+#         num_splits_one_extra = dim_size % sections
+#
+#         split_sizes = []
+#         for split_idx in range(sections):
+#             split_size = (
+#                 min_split_size + 1
+#                 if (split_idx < num_splits_one_extra)
+#                 else min_split_size
+#             )
+#             split_sizes.append(split_size)
+#
+#         return tuple(aten.split_with_sizes(a, split_sizes, dim=_dim))
+#     # Case 1 -- indices_or_sections is a sequence of integers or a 1D tensor describing the splits
+#     else:
+#         indices = indices_or_sections
+#         if isinstance(indices_or_sections, TensorLike):
+#             if indices_or_sections.ndim != 1:
+#                 msg = (
+#                     "tensor_split: non-scalar indices_or_sections tensors must have only one dimension, "
+#                     f"but received a tensor with {indices_or_sections.ndim} dimensions"
+#                 )
+#                 raise ValueError(msg)
+#
+#             indices = indices_or_sections.tolist()
+#
+#         indices = [0] + list(indices) + [a.shape[_dim]]
+#         split_sizes = [indices[i + 1] - indices[i] for i in range(len(indices) - 1)]
+#         return tuple(aten.split_with_sizes(a, split_sizes, dim=_dim))
 
