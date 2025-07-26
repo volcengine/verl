65c9a3577484e930967091c8b35bc144da85c972
zhangchi.usc1992 <zhangchi.usc1992@bytedance.com>
2025-07-26 14:41:50 +0800

    fix sft dataset

--- recipe/retool/run_qwen2_7b_sft.sh
+++ recipe/retool/run_qwen2_7b_sft.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 set -x
 
-nnodes=2
+nnodes=1
 nproc_per_node=8
-master_addr=
-master_port=
+master_addr=$ARNOLD_WORKER_0_HOST
+master_port=$ARNOLD_WORKER_0_PORT
+node_rank=$ARNOLD_ID
 
-experiment_name=multiturn-sft-qwen-2.5-32b-instruct
-HDFS_ROOT=${HDFS_ROOT:-$PWD}
-DATA_ROOT=${DATA_ROOT:-$PWD}
+project_name=retool
+experiment_name=multiturn-sft-qwen-2.5-7b-instruct
+# HDFS_ROOT=${HDFS_ROOT:-$PWD}
+# DATA_ROOT=${DATA_ROOT:-$PWD}
 
-TRAIN_DATA=$DATA_ROOT/dataset/wuxibin/ReTool-SFT/data/train-00000-of-00001.parquet
-EVAL_DATA=$DATA_ROOT/dataset/wuxibin/ReTool-SFT/data/train-00000-of-00001.parquet
-MODEL_PATH=$HDFS_ROOT/model/Qwen2.5-32B-Instruct
-SAVE_PATH=$DATA_ROOT/checkpoint/$experiment_name
+# TRAIN_DATA=$DATA_ROOT/dataset/wuxibin/ReTool-SFT/data/train-00000-of-00001.parquet
+# EVAL_DATA=$DATA_ROOT/dataset/wuxibin/ReTool-SFT/data/train-00000-of-00001.parquet
+# MODEL_PATH=$HDFS_ROOT/model/Qwen2.5-32B-Instruct
+# SAVE_PATH=$DATA_ROOT/checkpoint/$experiment_name
+
+TRAIN_DATA=/mnt/hdfs/zhangchi.usc1992_lf_lq/data/ReTool-SFT/data/train-00000-of-00001.parquet
+EVAL_DATA=/mnt/hdfs/zhangchi.usc1992_lf_lq/data/ReTool-SFT/data/train-00000-of-00001.parquet
+MODEL_PATH=/mnt/hdfs/zhangchi.usc1992_lf_lq/models/Qwen2.5-7B-Instruct
+SAVE_PATH=/mnt/hdfs/zhangchi.usc1992_ssd_hldy/open_verl/${project_name}/${exp_name}
 
 torchrun --nnodes=$nnodes \
      --nproc_per_node=$nproc_per_node \
