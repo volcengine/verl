ce33651b583976e599e28259b6954769a7e67ca6
kibitzing <kang-jingu@naver.com>
2025-07-28 04:04:19 +0000

    chore: apply formatting fixes from pre-commit

--- tests/utils/dataset/test_rl_collate_fn_on_cpu.py
+++ tests/utils/dataset/test_rl_collate_fn_on_cpu.py
@@ -16,6 +16,7 @@ import torch
 
 def test_rl_collate_fn():
     from verl.utils.dataset.rl_dataset import collate_fn
+
     max_prompt_length = 5
 
     test_data = [
@@ -29,12 +30,9 @@ def test_rl_collate_fn():
             # test string
             "ability": "math",
             # test dict
-            "reward_model": {
-                "ground_truth": 5,
-                "style": "rule"
-            },
+            "reward_model": {"ground_truth": 5, "style": "rule"},
             # test empty dict
-            "tools_kwargs": {}
+            "tools_kwargs": {},
         },
         {
             "input_ids": torch.randint(0, 10, (max_prompt_length,)),
@@ -43,10 +41,10 @@ def test_rl_collate_fn():
             "ability": "toolcall",
             "reward_model": {
                 "ground_truth": '[{"name": "rgb_to_cmyk", "arguments": {"r": 0, "g": 0, "b": 255}}]',
-                "style": "rule"
+                "style": "rule",
             },
-            "tools_kwargs": {}
-        }
+            "tools_kwargs": {},
+        },
     ]
 
     batch_size = len(test_data)
@@ -62,11 +60,13 @@ def test_rl_collate_fn():
         "raw_prompt_ids": list,
         "ability": str,
         "reward_model": dict,
-        "tools_kwargs": dict
+        "tools_kwargs": dict,
     }
 
     for key, dtype in expected_types.items():
-        assert batch[key].shape == (batch_size,), \
+        assert batch[key].shape == (batch_size,), (
             f"Expected shape {(batch_size,)} for '{key}', but got {batch[key].shape}"
-        assert isinstance(batch[key][0], dtype), \
+        )
+        assert isinstance(batch[key][0], dtype), (
             f"'{key}' should contain elements of type {dtype}, but got {type(batch[key][0])}"
+        )
