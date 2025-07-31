0d41f98f25163d5a3b3e5dd92abcf7ba0f6abfd1
haolinyan <haolinyan@tencent.com>
2025-07-31 15:44:39 +0800

    update training scripts

--- recipe/async_reward_agent/qwen2_7b_instruct_gsm8k.sh
+++ recipe/async_reward_agent/qwen2_7b_instruct_gsm8k.sh
@@ -33,24 +33,17 @@ huggingface-cli download "${MODEL_ID}" --local-dir "${MODEL_PATH}"
 adv_estimator=grpo
 
 use_kl_in_reward=False
-use_kl_loss=False
-kl_loss_coef=0.0
+use_kl_loss=True
+kl_loss_coef=0.001
 
 # Response length parameters
 max_prompt_length=512
-max_response_length=512
+max_response_length=1024
 
 # Training parameters
-loss_agg_mode="token-mean"
-train_prompt_bsz=64
+train_prompt_bsz=1024
 n_resp_per_prompt=5
-train_prompt_mini_bsz=16
-
-# Temperature parameters
-temperature=1.0
-top_p=1.0
-top_k=-1
-val_top_p=0.7
+train_prompt_mini_bsz=256
 
 n_gpus_training=${NUM_GPUS}
 
@@ -74,31 +67,24 @@ common_params=(
     algorithm.use_kl_in_reward=${use_kl_in_reward}
     actor_rollout_ref.actor.use_kl_loss=${use_kl_loss}
     actor_rollout_ref.actor.kl_loss_coef=${kl_loss_coef}
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl
     actor_rollout_ref.model.path="${MODEL_PATH}"
     actor_rollout_ref.actor.optim.lr=1e-6
     actor_rollout_ref.actor.ppo_mini_batch_size=${train_prompt_mini_bsz}
     actor_rollout_ref.actor.entropy_coeff=0
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.5
-    actor_rollout_ref.rollout.temperature=${temperature}
-    actor_rollout_ref.rollout.top_p=${top_p}
-    actor_rollout_ref.rollout.top_k=${top_k}
-    actor_rollout_ref.rollout.val_kwargs.temperature=${temperature}
-    actor_rollout_ref.rollout.val_kwargs.top_p=${val_top_p}
-    actor_rollout_ref.rollout.val_kwargs.top_k=${top_k}
-    actor_rollout_ref.rollout.val_kwargs.do_sample=True
-    actor_rollout_ref.rollout.val_kwargs.n=1
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6
+    actor_rollout_ref.rollout.max_num_batched_tokens=8192
     actor_rollout_ref.rollout.enable_chunked_prefill=True 
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 
     trainer.logger=['console']
     trainer.project_name='verl-test'
     trainer.experiment_name="${exp_name}"
     trainer.val_before_train=False
-    trainer.test_freq=-1
-    trainer.save_freq=-1
-    trainer.total_epochs=2
-    trainer.total_training_steps=2
+    trainer.test_freq=5
+    trainer.save_freq=20
+    trainer.total_epochs=15
     trainer.resume_mode=disable
     trainer.nnodes=1
     trainer.n_gpus_per_node=${n_gpus_training}
@@ -111,7 +97,7 @@ common_params=(
 if [ "${ACTOR_STRATEGY}" == "fsdp2" ]; then
     echo "Running with FSDP2 strategy..."
     # FSDP2 specific parameters
-    gen_tp=2
+    gen_tp=1
     ref_offload=True
     actor_offload=False
 
@@ -131,7 +117,7 @@ if [ "${ACTOR_STRATEGY}" == "fsdp2" ]; then
 elif [ "${ACTOR_STRATEGY}" == "megatron" ]; then
     echo "Running with Megatron strategy..."
     # Megatron specific parameters
-    gen_tp=2
+    gen_tp=1
     train_tp=2
     train_pp=1
     ref_offload=True
