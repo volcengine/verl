b4fdfcf6a625fc232ecb1d84a7ced6bc913f87ba
zhaochenyang <zhaochenyang20@gmail.com>
2025-08-05 14:24:16 -0700

    clean up codes

--- examples/sglang_multiturn/run_qwen2.5-3b_gsm8k_multiturn.sh
+++ examples/sglang_multiturn/run_qwen2.5-3b_gsm8k_multiturn.sh
@@ -47,13 +47,13 @@ python3 -m verl.trainer.main_ppo \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
-    trainer.project_name='benchmark_over_sample' \
+    trainer.project_name='benchmark_over_sample_2' \
     trainer.experiment_name=$EXPERIMENT_NAME \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
     trainer.test_freq=20 \
-    trainer.val_before_train=False \
+    trainer.val_before_train=True \
     data.train_files=$HOME/data/gsm8k/train.parquet \
     data.val_files=$HOME/data/gsm8k/test.parquet \
     actor_rollout_ref.rollout.multi_turn.tool_config_path="$PROJECT_DIR/examples/sglang_multiturn/config/tool_config/gsm8k_tool_config.yaml" \
