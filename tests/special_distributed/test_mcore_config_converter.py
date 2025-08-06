fdea4758587e0cfd560c53efd40333b9cde3365e
gaoziyuan <gaoziyuan.955@bytedance.com>
2025-08-06 18:58:16 +0800

    add unit test

--- tests/special_distributed/test_mcore_config_converter.py
+++ tests/special_distributed/test_mcore_config_converter.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 
 import megatron.core.parallel_state as mpu
 import torch
@@ -19,7 +20,7 @@ from megatron.core.transformer import MLATransformerConfig, TransformerConfig
 from transformers import AutoConfig, PretrainedConfig
 
 from verl.models.mcore import hf_to_mcore_config
-from verl.utils.distributed import initialize_global_process_group
+from verl.utils.distributed import destroy_global_process_group, initialize_global_process_group
 
 TEST_MODELS = [
     "Qwen/Qwen2.5-7B",  # Qwen2 dense
@@ -27,7 +28,7 @@ TEST_MODELS = [
     "deepseek-ai/deepseek-coder-1.3b-instruct",  # deepseek dense
     "Qwen/Qwen2-57B-A14B",  # Qwen2 moe
     "Qwen/Qwen3-30B-A3B",  # Qwen3 moe
-    "mistralai/Mixtral-8x7B-v0.1",  # Mixtral
+    # "mistralai/Mixtral-8x7B-v0.1",  # Mixtral # require authentication
     "deepseek-ai/DeepSeek-V3-Base",  # Deepseek V3
 ]
 
@@ -54,12 +55,20 @@ def check_config_converter_results(tf_config: TransformerConfig | MLATransformer
     assert tf_config.hidden_dropout == getattr(hf_config, "hidden_dropout", 0.0), (
         f"Hidden dropout mismatch: {tf_config.hidden_dropout} != {getattr(hf_config, 'hidden_dropout', 0.0)}"
     )
-    assert tf_config.kv_channels == getattr(hf_config, "head_dim", None), (
-        f"KV channels mismatch: {tf_config.kv_channels} != {getattr(hf_config, 'head_dim', None)}"
-    )
+    if getattr(hf_config, "head_dim", None) is not None:
+        assert tf_config.kv_channels == getattr(hf_config, "head_dim", None), (
+            f"Head dim mismatch: {tf_config.kv_channels} != {getattr(hf_config, 'head_dim', None)}"
+        )
     assert tf_config.layernorm_epsilon == hf_config.rms_norm_eps, (
         f"Layernorm epsilon mismatch: {tf_config.layernorm_epsilon} != {hf_config.rms_norm_eps}"
     )
+
+
+def modify_hf_config(name: str, hf_config: PretrainedConfig):
+    if name == "deepseek-ai/DeepSeek-V3-Base":
+        hf_config.num_nextn_predict_layers = 0
+        hf_config.quantization_config = None
+    return hf_config
 
 
 def test_mcore_config_converter():
@@ -80,9 +89,12 @@ def test_mcore_config_converter():
     )
     for model_name in TEST_MODELS:
         print(f"testing {model_name}")
-        hf_config = AutoConfig.from_pretrained(model_name)
+        hf_config = AutoConfig.from_pretrained(os.path.expanduser(f"~/configs/{model_name}/config.json"))
+        hf_config = modify_hf_config(model_name, hf_config)
         tf_config = hf_to_mcore_config(hf_config, torch.bfloat16)
         check_config_converter_results(tf_config, hf_config)
+
+    destroy_global_process_group()
 
 
 if __name__ == "__main__":
