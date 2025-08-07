1ce98ddd17b25e7e23e41da402bf1d63d57c29a7
Zhuorany <yzr1914001753@gmail.com>
2025-08-07 04:17:11 +0000

    1

--- examples/sglang_multiturn/run_qwen2_3b_dapo_multiturn.sh
+++ examples/sglang_multiturn/run_qwen2_3b_dapo_multiturn.sh
@@ -33,7 +33,7 @@ python3 -m verl.trainer.main_ppo \
     data.custom_cls.name=CustomRLHFDataset \
     custom_reward_function.path=$PROJECT_DIR/recipe/retool/retool.py \
     custom_reward_function.name=compute_score \
-    actor_rollout_ref.model.path=Qwen/Qwen2.5-3B-Instruct \
+    actor_rollout_ref.model.path=Qwen/Qwen3-4B-Instruct-2507 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.use_kl_loss=False \
