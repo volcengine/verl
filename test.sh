88fd15f9e3d51809976f12369e6bbbefa3634a45
changyi <changyiyang2023@gmail.com>
2025-08-07 23:57:49 +0000

    remove file

--- test.sh
+++
@@ -1,9 +0,0 @@
-# export HYDRA_FULL_ERROR=1
-export WANDB_API_KEY=8920a59faeab83c97b55c3cbe78618f11d0a1821
-# export NCCL_DEBUG=INFO
-
-# ./examples/sglang_multiturn/run_qwen2.5-3b_gsm8k_multiturn.sh
-
-# export CUDA_VISIBLE_DEVICES=4,5,6,7
-
-./examples/sglang_multiturn/run_qwen2.5-3b_gsm8k_multiturn.sh
