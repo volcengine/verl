c6ca4aeb7f4f8c7d3682e30c5e94063e14e056ea
Nemi <finetune@cgft.io>
2025-08-09 18:32:27 -0700

    update copyright header

--- verl/tools/benchmax_tool.py
+++ verl/tools/benchmax_tool.py
@@ -1,5 +1,5 @@
 # Copyright 2024 Bytedance Ltd. and/or its affiliates
-# Copyright 2023-2024 Benchmax Team
+# Copyright 2025 Benchmax Team
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
