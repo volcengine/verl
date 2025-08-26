e1df3d6d9daf2566be9b4fea5de42df48d51a990
Chi Zhang <zhangchi.usc1992@bytedance.com>
2025-08-26 14:38:52 +0800

    update

--- verl/workers/engine/megatron/engine_impl.py
+++ verl/workers/engine/megatron/engine_impl.py
@@ -11,32 +11,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import itertools
 import logging
 import os
-from typing import Callable
+from functools import partial
+from typing import Any, Callable
 
 import torch
 import torch.distributed
 from megatron.core import parallel_state as mpu
+from megatron.core.pipeline_parallel import get_forward_backward_func
 from omegaconf import OmegaConf
 
 from verl import DataProto
 from verl.trainer.config import CheckpointConfig
 from verl.utils.checkpoint.megatron_checkpoint_manager import MegatronCheckpointManager
-
-from verl.utils.device import get_device_id, get_device_name, get_nccl_backend, get_torch_device
-from verl.utils.model import load_mcore_dist_weights, load_megatron_gptmodel_weights
-from verl.workers.config import HFModelConfig, McoreEngineConfig, McoreOptimizerConfig
-
+from verl.utils.device import get_device_id
+from verl.utils.megatron.pipeline_parallel import make_batch_generator
+from verl.utils.megatron.tensor_parallel import vocab_parallel_entropy, vocab_parallel_log_probs_from_logits
 from verl.utils.megatron_utils import (
     load_megatron_model_to_gpu,
     load_megatron_optimizer,
     offload_megatron_model_to_cpu,
     offload_megatron_optimizer,
 )
+from verl.utils.model import load_mcore_dist_weights, load_megatron_gptmodel_weights
+from verl.utils.py_functional import append_to_dict
+from verl.utils.seqlen_balancing import get_reverse_idx, rearrange_micro_batches
+from verl.workers.config import HFModelConfig, McoreEngineConfig, McoreOptimizerConfig
 
 from ..base import BaseEngine, EngineRegistry
-
 from .utils import set_random_seed
 
 logger = logging.getLogger(__file__)
@@ -236,7 +241,7 @@ class MegatronEngine(BaseEngine):
         Zero out gradients of all parameters before starting a new backward pass.
         """
         self.optimizer.zero_grad()
-            # use use_contiguous_buffers_in_local_ddp and no overlap_dp_param_comm
+        # use use_contiguous_buffers_in_local_ddp and no overlap_dp_param_comm
         for chunk in self.module:
             # if use distributed optimizer, zero grad buffer will be handled by optimizer
             chunk.zero_grad_buffer()
@@ -248,7 +253,7 @@ class MegatronEngine(BaseEngine):
         Returns:
             grad_norm (float): The norm of the gradients before clipping or update.
         """
-        update_successful, grad_norm, num_zeros_in_grad = self.actor_optimizer.step()
+        update_successful, grad_norm, num_zeros_in_grad = self.optimizer.step()
 
         if update_successful:
             # allgather already execute in optimizer.step in new megatron
@@ -266,6 +271,7 @@ class MegatronEngine(BaseEngine):
             current_lr (float or list[float]): Updated learning rate(s).
         """
         from verl.utils.megatron.optimizer import get_megatron_last_lr
+
         self.lr_scheduler.step(1)
         return get_megatron_last_lr(self.optimizer)
 
@@ -304,7 +310,14 @@ class MegatronEngine(BaseEngine):
             global_step: Integer training step number for naming.
             max_ckpt_to_keep: Maximum number of recent checkpoints to retain.
         """
-        raise NotImplementedError
+        if self._is_offload_param:
+            load_megatron_model_to_gpu(self.module, load_grad=True)
+        self.checkpoint_mananager.save_checkpoint(
+            local_path=local_path, hdfs_path=hdfs_path, global_step=global_step, max_ckpt_to_keep=max_ckpt_to_keep
+        )
+        torch.distributed.barrier()
+        if self._is_offload_param:
+            offload_megatron_model_to_cpu(self.module)
 
     def load_checkpoint(self, local_path, hdfs_path=None, del_local_after_load=True):
         """
@@ -324,8 +337,6 @@ class MegatronEngine(BaseEngine):
             offload_megatron_model_to_cpu(self.module)
         if self._is_offload_optimizer:
             offload_megatron_optimizer(self.optimizer)
-
-
 
 
 class EngineEvalModeCtx:
@@ -368,3 +379,228 @@ class EngineTrainModeCtx:
         if self.engine._is_offload_optimizer:
             offload_megatron_optimizer(optimizer=self.engine.optimizer)
         self.engine.mode = None
+
+
+class MegatronEngineForCausalLM(MegatronEngine):
+    def forward_backward_batch(self, data: DataProto, loss_function: Callable, forward_only=False) -> Any:
+        use_dynamic_bsz = data.meta_info.get("use_dynamic_bsz", True)
+        use_fused_kernels = data.meta_info.get("use_fused_kernels", False)
+        calculate_entropy = data.meta_info.get("calculate_entropy", False)
+
+        if use_dynamic_bsz:
+            assert "max_token_len_per_gpu" in data.meta_info, (
+                "max_token_len_per_gpu must be set when use_dynamic_bsz is True"
+            )
+            max_token_len_per_gpu = data.meta_info.get("max_token_len_per_gpu")
+            max_token_len = max_token_len_per_gpu * self.engine_config.context_parallel_size
+        else:
+            micro_batch_size = data.meta_info.get("micro_batch_size")
+
+        # step 1: split batch data into micro-batches
+        data = data.to(get_device_id())
+        data.batch = data.batch.contiguous()
+        mini_batch = data
+        mini_batch.to("cpu")
+        # split into micro-batches
+        mini_batch.batch["attention_mask"] = mini_batch.batch["attention_mask"].to(bool)
+        self.has_multi_modal_inputs = "multi_modal_inputs" in mini_batch.non_tensor_batch.keys()
+        if self.has_multi_modal_inputs:
+            mini_batch.batch["multi_modal_inputs"] = mini_batch.non_tensor_batch["multi_modal_inputs"]
+            mini_batch.batch["multi_modal_inputs_idx"] = torch.Tensor(
+                list(range(len(mini_batch.non_tensor_batch["multi_modal_inputs"])))
+            ).to(torch.int64)
+
+        if mini_batch.batch["position_ids"].dim() == 3:  # qwen2vl mrope [bs, 3, seq_len]
+            mini_batch.batch["position_ids"] = mini_batch.batch["position_ids"][
+                :, 0
+            ]  # mcore patch recompute qwen2vl's pos ids during forward
+
+        indices = None
+        temperature = data.meta_info["temperature"]
+        if use_dynamic_bsz:
+            assert max_token_len is not None, "max_token_len must be set when use_dynamic_bsz is True"
+            vpp_size = mpu.get_virtual_pipeline_model_parallel_world_size()
+            if vpp_size is not None and vpp_size > 1:
+                microbatch_group_size_per_vp_stage = self.tf_config.microbatch_group_size_per_vp_stage
+                micro_batches, indices = rearrange_micro_batches(
+                    batch=mini_batch.batch,
+                    num_batches_divided_by=microbatch_group_size_per_vp_stage,
+                    max_token_len=max_token_len,
+                )
+                assert len(micro_batches) % self.tf_config.microbatch_group_size_per_vp_stage == 0, (
+                    f"micro_batches {micro_batches} must be divisible by microbatch_group_size_per_vp_stage "
+                    f"{microbatch_group_size_per_vp_stage} for megatron backend"
+                )
+            else:
+                micro_batches, indices = rearrange_micro_batches(batch=mini_batch.batch, max_token_len=max_token_len)
+        else:
+            assert micro_batch_size is not None, (
+                "micro_batch_size is needed to be passed in when not using dynamic batch size"
+            )
+            micro_batches = mini_batch.batch.split(micro_batch_size)
+        # compute input shapes for pp stages
+        n_micro_batch = len(micro_batches)
+
+        forward_backward_func = get_forward_backward_func()
+
+        def loss_func(output, data):
+            # For memory efficiency
+            # We move calculation of entropy to compute_log_probs, forward_only == True
+            device = output["log_probs"].device
+
+            responses = data["responses"]
+            response_length = responses.size(1)
+
+            log_prob = output["log_probs"][:, -response_length - 1 : -1].contiguous()
+            model_output = {"log_probs": log_prob}
+            if calculate_entropy:
+                entropy = output["entropy"][:, -response_length - 1 : -1].contiguous()
+                model_output["entropy"] = entropy
+
+            if forward_only:
+                # for inference
+                return torch.tensor(1.0, device=device), model_output
+
+            # for training
+            # note that this loss function can be swapped with other loss functions such as SFT
+            policy_loss, metrics = loss_function(model_output, data)
+
+            # return loss and stats
+            return policy_loss, metrics
+
+        def forward_step(batch_iter, model):
+            batch = next(batch_iter)
+            batch = batch.to(get_device_id())
+            batch = batch.contiguous()
+
+            input_ids = batch["input_ids"]
+            attention_mask = batch["attention_mask"].to(bool)
+            position_ids = batch["position_ids"]
+
+            multi_modal_inputs = {}
+            if "multi_modal_inputs" in batch:
+                for key in batch["multi_modal_inputs"][0].keys():
+                    idxs = batch["multi_modal_inputs_idx"]
+                    mmi = batch["multi_modal_inputs"]
+                    multi_modal_inputs[key] = torch.cat(
+                        [mmi[idx].get(key) for idx in idxs if mmi[idx].get(key) is not None], dim=0
+                    )
+            responses = batch["responses"]
+            response_length = responses.size(1)
+            label = position_ids.clone()
+            label[:, -response_length - 1 : -1] = responses
+            label_mask = attention_mask.clone()
+            label_mask[:, : -response_length - 1] = False
+            label_mask[:, -1] = False
+
+            from verl.models.mcore import get_mcore_forward_fn, get_mcore_forward_fused_fn
+
+            if use_fused_kernels:
+                forward_fn = get_mcore_forward_fused_fn(self.model_config.hf_config)
+                # return dict of [logits, entropy]
+                output = forward_fn(
+                    model,
+                    input_ids,
+                    position_ids,
+                    attention_mask,
+                    sequence_parallel=self.tf_config.sequence_parallel,
+                    multi_modal_inputs=multi_modal_inputs,
+                    labels=label,
+                    labels_mask=label_mask,
+                    temperature=temperature,
+                )
+            else:
+                forward_fn = get_mcore_forward_fn(self.model_config.hf_config)
+
+                def logits_processor(logits, label, label_mask):
+                    assert logits.shape[:2] == label.shape[:2]
+                    assert label.shape == label_mask.shape
+                    logits.div_(temperature)
+                    ret = {}
+                    if calculate_entropy:
+                        logits_bak = logits.clone()
+                        if torch.distributed.get_rank() == 0:
+                            logger.warning_once(
+                                "For memory-efficient computation, enable fused kernels via "
+                                "`actor_rollout_ref.model.use_fused_kernels=True`. "
+                                "The current `clone()` operation ensures correctness but increases memory usage."
+                            )
+                        entropy = vocab_parallel_entropy(logits)
+                        ret["entropy"] = entropy
+                    else:
+                        logits_bak = logits
+                    log_probs = vocab_parallel_log_probs_from_logits(logits_bak, label)
+                    log_probs = log_probs.masked_fill(~label_mask, 0.0)
+                    ret["log_probs"] = log_probs
+                    return ret
+
+                logits_processor_args = {"label": label, "label_mask": label_mask}
+                output = forward_fn(
+                    model,
+                    input_ids,
+                    attention_mask,
+                    position_ids,
+                    sequence_parallel=self.tf_config.sequence_parallel,
+                    multi_modal_inputs=multi_modal_inputs,
+                    logits_processor=logits_processor,
+                    logits_processor_args=logits_processor_args,
+                )
+
+            return output, partial(loss_func, data=batch)
+
+        # batch should be a list of batches inside micro-batches
+        batch_generator = make_batch_generator(micro_batches, vpp_size=len(self.module))
+
+        # TODO: we may use the new schedule instead
+        # for flash-attn: (seq_len, batch_size, hidden_size) = (mbs*seq_len, 1, hidden_size)
+        losses_reduced = forward_backward_func(
+            forward_step_func=forward_step,
+            data_iterator=batch_generator,
+            model=self.module,
+            num_microbatches=n_micro_batch,
+            seq_length=1,  # the communication shape is obtained via p2p comm
+            micro_batch_size=1,  # the communication shape is obtained via p2p comm
+            forward_only=forward_only,
+        )
+        # loss_reduces contains the stats returned from loss_func
+        if self.has_multi_modal_inputs:
+            data.batch.pop("multi_modal_inputs")
+            data.batch.pop("multi_modal_inputs_idx")
+            data.non_tensor_batch.pop("multi_modal_inputs")
+
+        if mpu.is_pipeline_last_stage(ignore_virtual=True):
+            if forward_only:
+                # losses_reduced is a list of dict containing entropy and logprobs for each micro-batch
+                # reorder entropy and logprobs. Return None for other pp ranks
+                # only on last rank. It should be on every tp rank
+                log_probs = [o["log_probs"] for o in losses_reduced]  # (bs, seq_size)
+                log_probs = torch.cat(log_probs, dim=0).to(torch.float32)
+
+                if calculate_entropy:
+                    entropys = torch.cat([o["entropy"] for o in losses_reduced], dim=0)
+                    entropys = entropys.to(torch.float32)
+
+                if use_dynamic_bsz:
+                    indices = list(itertools.chain.from_iterable(indices))
+                    assert len(indices) == log_probs.size(0), f"{len(indices)} vs. {log_probs.size()}"
+                    revert_indices = torch.tensor(get_reverse_idx(indices), dtype=torch.long)
+                    log_probs = log_probs[revert_indices]
+                    if calculate_entropy:
+                        assert len(indices) == entropys.size(0), f"{len(indices)} vs. {entropys.size()}"
+                        entropys = entropys[revert_indices]
+
+                output = {
+                    "log_probs": log_probs,
+                    "entropys": entropys,
+                }
+                return output
+
+            else:
+                metrics = {}
+                # combine metrics of each micro-batch
+                metric_micro_batch = losses_reduced
+                for metric in metric_micro_batch:
+                    # Note that o[0] is metrics, o[1] is entropy, o[2] is response_mask
+                    append_to_dict(metrics, metric)  # append the metric from this micro-batch to global metrics.
+
+                return metrics
