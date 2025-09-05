43d523adbdc56081988dc9f4e0d2328482b6940d
zhangchi.usc1992 <zhangchi.usc1992@bytedance.com>
2025-09-05 19:17:57 +0800

    update tests

--- tests/special_e2e/sft/compare_sft_engine_results.py
+++ tests/special_e2e/sft/compare_sft_engine_results.py
@@ -1,22 +1,41 @@
 import os
-
+import json
+import torch
 
 def get_result(file):
     file = os.path.expanduser(file)
+    result = []
     with open(file, "r") as f:
         lines = f.readlines()
         for line in lines:
-            if "step:" in line:
-                result = line.split(" - ")
-
-                loss_result = result[1]
-                grad_norm = result[2]
-
-                from IPython import embed
-                embed()
+            result.append(json.loads(line))
+    return result
 
 
+def compare_results(golden_results, other_result):
+    golden_loss = golden_results[0]['data']['train/loss']
+    golden_grad_norm = golden_results[0]['data']['train/grad_norm']
+
+    loss = other_result[0]['data']['train/loss']
+    grad_norm = other_result[0]['data']['train/grad_norm']
+
+    torch.testing.assert_close(golden_loss, loss, atol=1e-2, rtol=1e-2)
+    torch.testing.assert_close(golden_grad_norm, grad_norm, atol=1e-4, rtol=1e-2)
 
 
 if __name__ == "__main__":
-    get_result("~/verl/test/log/golden.log")
+    golden_results = get_result("~/verl/test/log/golden.jsonl")
+
+    # get all other results
+    other_results = {}
+    # walk through all files in ~/verl/test/log
+    for file in os.listdir(os.path.expanduser("~/verl/test/log/verl_sft_test")):
+        if file.endswith(".jsonl"):
+            other_results[file] = get_result(os.path.join(os.path.expanduser("~/verl/test/log/verl_sft_test"), file))
+
+    # # compare results
+    for file, other_result in other_results.items():
+        print(f'compare results {file}')
+        compare_results(golden_results, other_result)
+
+    print("All results are close to golden results")
