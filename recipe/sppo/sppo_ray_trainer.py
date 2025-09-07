9216c7b473e1d02a9ba29cb94d367e9162583fd1
Tialo <tialox@yandex.ru>
2025-09-07 13:15:44 +0000

    add missing finish

--- recipe/sppo/sppo_ray_trainer.py
+++ recipe/sppo/sppo_ray_trainer.py
@@ -354,7 +354,9 @@ class RaySPPOTrainer(RayPPOTrainer):
             if is_last_step:
                 pprint(f"Final validation metrics: {last_val_metrics}")
                 progress_bar.close()
+                logger.finish()
                 return
 
             progress_bar.update(1)
             self.global_steps += 1
+        logger.finish()
