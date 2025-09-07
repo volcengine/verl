2e67e21083301fc967206eaa23302e6da424c953
HaochenYuan <106647990+HaochenYuan@users.noreply.github.com>
2025-09-07 20:20:51 +0800

    Update run_deepseek671b_math_megatron_80gb.sh

--- examples/grpo_trainer/run_deepseek671b_math_megatron_80gb.sh
+++ examples/grpo_trainer/run_deepseek671b_math_megatron_80gb.sh
@@ -51,6 +51,8 @@ infer_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 3))
 # RAY_ADDRESS='auto' ray job submit --working-dir . --
 python3 -m verl.trainer.main_ppo --config-path=./config --config-name='ppo_megatron_trainer'\
     algorithm.adv_estimator=grpo \
+    algorithm.use_kl_in_reward=False \
+    algorithm.kl_ctrl.kl_coef=0.0 \
     data.train_files="$train_files" \
     data.val_files="$test_files" \
     data.train_batch_size=512 \
@@ -62,8 +64,9 @@ python3 -m verl.trainer.main_ppo --config-path=./config --config-name='ppo_megat
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.use_torch_compile=False \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
