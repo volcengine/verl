76839b614b64655cf503814f1da9a7585fbc64ae
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2025-09-12 18:59:21 +0200

    Minimum `transformers` version for Apertus `test_transformers_ulysses.py`

--- tests/models/test_transformers_ulysses.py
+++ tests/models/test_transformers_ulysses.py
@@ -18,9 +18,11 @@ from dataclasses import dataclass
 import pytest
 import torch
 import torch.distributed
+import transformers
 from flash_attn.bert_padding import index_first_axis, rearrange, unpad_input
+from packaging import version
 from torch.distributed import init_device_mesh
-from transformers import ApertusConfig, AutoModelForCausalLM, LlamaConfig, PretrainedConfig, Qwen2Config
+from transformers import AutoModelForCausalLM, LlamaConfig, PretrainedConfig, Qwen2Config
 
 from verl.models.transformers.monkey_patch import apply_monkey_patch
 from verl.protocol import DataProto
@@ -46,7 +48,7 @@ class SequenceParallelConfig:
 
 
 def test_configs():
-    return [
+    configs = [
         SequenceParallelConfig(
             LlamaConfig(num_hidden_layers=2, num_attention_heads=32, num_key_value_heads=32), sp_size=8, is_valid=True
         ),
@@ -66,12 +68,20 @@ def test_configs():
         SequenceParallelConfig(
             Qwen2Config(num_hidden_layers=2, num_attention_heads=32, num_key_value_heads=4), sp_size=8, is_valid=True
         ),
-        SequenceParallelConfig(
-            ApertusConfig(num_hidden_layers=2, num_attention_heads=32, num_key_value_heads=32, hidden_size=4096),
-            sp_size=8,
-            is_valid=True,
-        ),
     ]
+
+    if version.parse(transformers.__version__) >= version.parse("4.56.0"):
+        from transformers import ApertusConfig
+
+        configs.append(
+            SequenceParallelConfig(
+                ApertusConfig(num_hidden_layers=2, num_attention_heads=32, num_key_value_heads=32, hidden_size=4096),
+                sp_size=8,
+                is_valid=True,
+            )
+        )
+
+    return configs
 
 
 def sync_model_parameters_global(layer):
