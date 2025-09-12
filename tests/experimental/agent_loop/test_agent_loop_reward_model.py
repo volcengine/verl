071b761e9612796fd1fdaaeacd00efa76d6ae21a
wuxibin <wuxibin@bytedance.com>
2025-09-12 14:39:11 +0800

    disable cuda graph to acclerate ci

--- tests/experimental/agent_loop/test_agent_loop_reward_model.py
+++ tests/experimental/agent_loop/test_agent_loop_reward_model.py
@@ -45,6 +45,7 @@ def test_agent_loop_compute_score_with_model():
     config.actor_rollout_ref.actor.use_dynamic_bsz = True
     config.actor_rollout_ref.rollout.name = os.environ["ROLLOUT_NAME"]
     config.actor_rollout_ref.rollout.mode = "async"
+    config.actor_rollout_ref.rollout.enforce_eager = True
     config.actor_rollout_ref.rollout.prompt_length = 1024
     config.actor_rollout_ref.rollout.response_length = 4096
     config.actor_rollout_ref.rollout.skip_tokenizer_init = True
