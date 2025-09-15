a4bb6c71952f00ac55267874b1993407e342c0ec
songyy29 <103621974+songyy29@users.noreply.github.com>
2025-09-16 01:21:15 +0800

    Update geo3k.py

--- examples/data_preprocess/geo3k.py
+++ examples/data_preprocess/geo3k.py
@@ -24,7 +24,7 @@ from verl.utils.hdfs_io import copy, makedirs
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--local_dir", default="~/data/")
+    parser.add_argument("--local_dir", default="~/data/geo3k")
     parser.add_argument("--hdfs_dir", default=None)
     parser.add_argument("--local_dataset_path", default=None)
 
