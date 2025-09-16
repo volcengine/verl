b5301fe1a00809131db6774cba99be3972cb02da
pqhgit <39796558+pqhgit@users.noreply.github.com>
2025-09-16 17:21:53 +0800

    Update dapo_ray_trainer.py
    test

--- recipe/dapo/dapo_ray_trainer.py
+++ recipe/dapo/dapo_ray_trainer.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-FSDP PPO Trainer with Ray-based single controller.
+FSDP PPO Trainer with Ray-based single controller. 
+
 This trainer supports model-agonistic model initialization with huggingface
 """
 
