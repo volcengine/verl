b07df31faedc7d5adb3bb0931a8023ef204b2cae
朱学凯 <xuekaizhu@whu.edu.cn>
2025-10-02 16:47:55 +0800

    update

--- recipe/flowrl/prepare_data.sh
+++ recipe/flowrl/prepare_data.sh
@@ -2,16 +2,19 @@
 set -uxo pipefail
 
 export DATA_DIR=${DATA_DIR:-"downloads"}
-export TRAIN_FILE=${TRAIN_FILE:-"${DATA_DIR}/dapo-math-17k.parquet"}
-export TEST_FILE=${TEST_FILE:-"${DATA_DIR}/aime-2024.parquet"}
-export OVERWRITE=${OVERWRITE:-0}
 
-mkdir -p "${DATA_DIR}"
+# Download DAPO-Math-17k dataset
+DATASET_NAME_TRAIN="BytedTsinghua-SIA/DAPO-Math-17k"
+huggingface-cli download $DATASET_NAME_TRAIN \
+  --repo-type dataset \
+  --resume-download \
+  --local-dir ${DATA_DIR}/${DATASET_NAME_TRAIN} \
+  --local-dir-use-symlinks False
 
-if [ ! -f "${TRAIN_FILE}" ] || [ "${OVERWRITE}" -eq 1 ]; then
-  wget -O "${TRAIN_FILE}" "https://huggingface.co/datasets/BytedTsinghua-SIA/DAPO-Math-17k/resolve/main/data/dapo-math-17k.parquet?download=true"
-fi
-
-if [ ! -f "${TEST_FILE}" ] || [ "${OVERWRITE}" -eq 1 ]; then
-  wget -O "${TEST_FILE}" "https://huggingface.co/datasets/BytedTsinghua-SIA/AIME-2024/resolve/main/data/aime-2024.parquet?download=true"
-fi
+# Download AIME-2024 dataset
+DATASET_NAME_TEST="BytedTsinghua-SIA/AIME-2024"
+huggingface-cli download $DATASET_NAME_TEST \
+  --repo-type dataset \
+  --resume-download \
+  --local-dir ${DATA_DIR}/${DATASET_NAME_TEST} \
+  --local-dir-use-symlinks False
