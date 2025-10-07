22ddc7af77ca5c0b848a3d846d36a4f6a6cfe6f4
朱学凯 <xuekaizhu@whu.edu.cn>
2025-10-07 18:37:45 +0800

    update

--- recipe/flowrl/prepare/prepare_model.sh
+++ recipe/flowrl/prepare/prepare_model.sh
@@ -5,6 +5,6 @@ MODEL_NAME=Qwen/Qwen2.5-7B
 huggingface-cli download $MODEL_NAME \
   --repo-type model \
   --resume-download \
-  --local-dir downloads/$MODEL_NAME \
+  --local-dir downloads/models/$MODEL_NAME \
   --local-dir-use-symlinks False \
   --exclude *.pth
