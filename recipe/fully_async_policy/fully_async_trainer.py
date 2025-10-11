97615b4b3d13de63d317bd58c31142a95a0c24be
wangshulin02 <953550366@qq.com>
2025-10-11 15:52:50 +0800

    add require_batches config param

--- recipe/fully_async_policy/fully_async_trainer.py
+++ recipe/fully_async_policy/fully_async_trainer.py
@@ -97,8 +97,9 @@ class FullyAsyncTrainer(FullyAsyncRayPPOTrainer):
         self.progress_bar = None
         self.trigger_parameter_sync_step = config.async_training.trigger_parameter_sync_step
 
-        # required_samples use ppo_mini_batch_size as the minimum number of samples.
-        self.required_samples = config.actor_rollout_ref.actor.ppo_mini_batch_size
+        # required_samples use ppo_mini_batch_size*require_batches as the minimum number of samples.
+        self.require_batches = config.async_training.require_batches
+        self.required_samples = config.actor_rollout_ref.actor.ppo_mini_batch_size * self.require_batches
         total_gpus = (
             config.trainer.nnodes * config.trainer.n_gpus_per_node
             + config.rollout.nnodes * config.rollout.n_gpus_per_node
