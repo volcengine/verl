57bc097469e23abfbdbc6ccea75b4fc7bfc650a4
朱学凯 <xuekaizhu@whu.edu.cn>
2025-10-17 13:53:27 +0800

    update

--- recipe/flowrl/run_flowrl_vanilla_qwen2.5_7b.sh
+++ recipe/flowrl/run_flowrl_vanilla_qwen2.5_7b.sh
@@ -2,7 +2,7 @@
 set -xeuo pipefail
 
 project_name='FlowRL'
-exp_name='FlowRL-vanilla-Qwen2.5-7B-source_load_Z_1016'
+exp_name='FlowRL-vanilla-Qwen2.5-7B-new-load-z-1017'
 
 # Algorithm settings
 adv_estimator=grpo
