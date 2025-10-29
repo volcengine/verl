0dd761e10e5fd3e3acb5f3dd8c2c0d6599d92093
Your Name <you@example.com>
2025-10-29 10:50:39 +0800

    .

--- tests/special_npu/run_qwen3_30b_dapo_mindspeed.sh
+++ tests/special_npu/run_qwen3_30b_dapo_mindspeed.sh
@@ -1,23 +1,38 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
+ray stop --force
+export RAY_DEBUG_POST_MORTEM=1
+ray start --head --port=6666 --dashboard-port=8888 --ray-debugger-external
 
 NUM_GPUS=${NUM_GPUS:-8}
 
 MODEL_ID=${MODEL_ID:-Qwen/Qwen3-30B-Instruct}
 MODEL_PATH=${MODEL_PATH:-${HOME}/models/${MODEL_ID}}
-huggingface-cli download "${MODEL_ID}" --local-dir "${MODEL_PATH}"
 
 USE_DIST_CKPT=${USE_DIST_CKPT:-False}
+USE_DUMMY_MODEL=${USE_DUMMY_MODEL:-False}
 DIST_CKPT_PATH=${DIST_CKPT_PATH:-${HOME}/dist_ckpt/qwen3_30b_dapo_mindspeed}
-if [[ ${USE_DIST_CKPT} == "True" ]]; then
-    if [[ ${USE_DUMMY_MODEL} == "True" ]]; then
-        DIST_CKPT_PATH=${HOME}/dist_ckpt_dummy/${MODEL_ID}
+
+if [[ ${USE_DUMMY_MODEL} == "True" ]]; then
+    if [ -z "${DUMMY_MODEL_CONFIG_PATH}"  ]; then
+        echo "[ERROR] DUMMY_MODEL_CONFIG_PATH not set"
+        exit 1
     fi
-    python scripts/converter_hf_to_mcore.py \
+    DUMMY_MODEL_PATH=${DUMMY_MODEL_PATH:-${HOME}/dummy_models/${MODEL_ID}}
+    python scripts/init_random_model.py \
+        --hf_model_path "${MODEL_PATH}" \
+        --new_config_path "${DUMMY_MODEL_CONFIG_PATH}" \
+        --output_path "${DUMMY_MODEL_PATH}"
+    MODEL_PATH=${DUMMY_MODEL_PATH}
+    DIST_CKPT_PATH=${HOME}/dist_ckpt_dummy/${MODEL_ID}
+fi
+
+if [[ ${USE_DIST_CKPT} == "True" ]]; then
+    torchrun --nproc_per_node 4 --nnodes 1 scripts/converter_hf_to_mcore.py \
         --hf_model_path ${MODEL_PATH} \
         --output_path ${DIST_CKPT_PATH}
 fi
-
+export RAY_DEBUG_POST_MORTEM
 exp_name='CI-DAPO-Qwen3-30B-megatron'
 
 # Megatron backen
@@ -33,9 +48,6 @@ use_kl_in_reward=False
 use_kl_loss=False
 kl_loss_coef=0.0
 
-clip_ratio_low=0.2
-clip_ratio_high=0.28
-
 max_prompt_length=1024
 max_response_length=2048
 enable_overlong_buffer=True
@@ -44,27 +56,13 @@ overlong_penalty_factor=1.0
 
 loss_agg_mode="token-mean"
 
-enable_filter_groups=False # True may cause CI Failded, since answer be cut by max_response_length
-filter_groups_metric=acc
 max_num_gen_batches=10
 train_prompt_bsz=16
 gen_prompt_bsz=$((train_prompt_bsz * 2))
 n_resp_per_prompt=4
 train_prompt_mini_bsz=2
 
-# Paths
-MODEL_PATH=${MODEL_PATH:-"${RAY_DATA_HOME}/models/Qwen3-30B-A3B"}
-
-MCORE_MODEL_PATH=${MCORE_MODEL_PATH:-"${RAY_DATA_HOME}/models/Qwen3-30B-A3B-dist_ckpt"}
-CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${project_name}/${exp_name}"}
-
-# Algorithm
-temperature=1.0
-top_p=1.0
-top_k=-1 # 0 for HF rollout, -1 for vLLM rollout
-
 # Performance Related Parameter
-sp_size=8
 use_dynamic_bsz=True
 actor_ppo_max_token_len=$(((max_prompt_length + max_response_length)))
 infer_ppo_max_token_len=$(((max_prompt_length + max_response_length)))
@@ -72,9 +70,8 @@ offload=True
 
 max_num_batched_tokens=$((max_prompt_length + max_response_length))
 
-
-
 python3 -m recipe.dapo.main_dapo \
+    --config-path=config \
     --config-name="dapo_megatron_trainer" \
     data.filter_overlong_prompts=False \
     data.train_files=${HOME}/data/gsm8k/train.parquet \
@@ -93,13 +90,13 @@ python3 -m recipe.dapo.main_dapo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.actor.use_kl_loss=${use_kl_loss} \
     actor_rollout_ref.actor.kl_loss_coef=${kl_loss_coef} \
-    actor_rollout_ref.actor.clip_ratio_low=${clip_ratio_low} \
-    actor_rollout_ref.actor.clip_ratio_high=${clip_ratio_high} \
+    actor_rollout_ref.actor.clip_ratio_low=0.2 \
+    actor_rollout_ref.actor.clip_ratio_high=0.28 \
     actor_rollout_ref.actor.clip_ratio_c=10.0 \
     actor_rollout_ref.actor.ppo_epochs=1 \
-    algorithm.filter_groups.enable=${enable_filter_groups} \
+    algorithm.filter_groups.enable=False \
     algorithm.filter_groups.max_num_gen_batches=${max_num_gen_batches} \
-    algorithm.filter_groups.metric=${filter_groups_metric} \
+    algorithm.filter_groups.metric=acc \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.use_dynamic_bsz=${use_dynamic_bsz} \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
@@ -136,19 +133,14 @@ python3 -m recipe.dapo.main_dapo \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.actor.loss_agg_mode=${loss_agg_mode} \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=${gen_tp} \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.enable_chunked_prefill=True \
     actor_rollout_ref.rollout.enable_prefix_caching=False \
     actor_rollout_ref.rollout.max_num_batched_tokens=${max_num_batched_tokens} \
     actor_rollout_ref.rollout.max_model_len=$((max_prompt_length + max_response_length)) \
-    actor_rollout_ref.rollout.temperature=${temperature} \
-    actor_rollout_ref.rollout.top_p=${top_p} \
-    actor_rollout_ref.rollout.top_k=${top_k} \
-    actor_rollout_ref.rollout.val_kwargs.temperature=${temperature} \
-    actor_rollout_ref.rollout.val_kwargs.top_p=${top_p} \
-    actor_rollout_ref.rollout.val_kwargs.top_k=${top_k} \
-    actor_rollout_ref.rollout.val_kwargs.do_sample=True \
-    actor_rollout_ref.rollout.val_kwargs.n=1 \
+    actor_rollout_ref.rollout.temperature=1.0 \
+    actor_rollout_ref.rollout.top_p=1.0 \
+    actor_rollout_ref.rollout.top_k=-1 \
     actor_rollout_ref.rollout.enforce_eager=True \
     actor_rollout_ref.rollout.free_cache_engine=True \
     actor_rollout_ref.ref.megatron.use_dist_checkpointing=True \
@@ -166,7 +158,6 @@ python3 -m recipe.dapo.main_dapo \
     trainer.save_freq=-1 \
     trainer.total_epochs=1 \
     trainer.total_training_steps=1 \
-    trainer.default_local_dir="${CKPTS_DIR}" \
     trainer.device="npu" \
     actor_rollout_ref.nccl_timeout=14400 \
     actor_rollout_ref.actor.use_torch_compile=False \
