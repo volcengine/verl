4339fde0cac145002c3fe1c5e6e2022b225340c4
wuxibin <wuxibin@bytedance.com>
2025-11-12 14:58:25 +0800

    fix ci

--- verl/utils/megatron/dist_checkpointing.py
+++ verl/utils/megatron/dist_checkpointing.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import torch
+import transformer_engine as te
 from megatron.core import dist_checkpointing, mpu
 from megatron.core.dist_checkpointing.serialization import (
     get_default_load_sharded_strategy,
@@ -53,6 +54,7 @@ def load_dist_checkpointing(sharded_state_dict, ckpt_dir):
 
     # Load model sharded state dicts
     torch.serialization.add_safe_globals([torch.optim.AdamW])
+    torch.serialization.add_safe_globals([te.pytorch.optimizers.fused_adam.FusedAdam])
     state_dict = dist_checkpointing.load(sharded_state_dict, ckpt_dir, sharded_strategy=load_strategy)
 
     return state_dict
