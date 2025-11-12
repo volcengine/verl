be1fec226ff8466763c0ee0aceaf044b5ada6e88
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2025-11-12 02:08:03 +0100

    Recipes

--- recipe/prime/prime_dp_rm.py
+++ recipe/prime/prime_dp_rm.py
@@ -19,12 +19,12 @@ import itertools
 
 import torch
 import torch.distributed
-from flash_attn.bert_padding import index_first_axis, pad_input, rearrange, unpad_input
 from torch import nn, optim
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 
 import verl.utils.torch_functional as verl_F
 from verl import DataProto
+from verl.utils.attention_utils import index_first_axis, pad_input, rearrange, unpad_input
 from verl.utils.device import get_device_name
 from verl.utils.py_functional import append_to_dict
 from verl.utils.seqlen_balancing import get_reverse_idx, rearrange_micro_batches
