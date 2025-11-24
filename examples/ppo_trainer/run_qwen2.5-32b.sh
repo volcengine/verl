7ebd78539b0211a91b4e8347e59ddf0521a4c244
khazic <khazzz1c@gmail.com>
2025-11-24 16:48:10 +0800

    a faulty duplicate setting was deleted

--- examples/ppo_trainer/run_qwen2.5-32b.sh
+++ examples/ppo_trainer/run_qwen2.5-32b.sh
@@ -18,7 +18,7 @@ python3 -m verl.trainer.main_ppo \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-32B-Instruct \
-    actor_rollout_ref.model.enable_gradient_checkpointing=False \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
