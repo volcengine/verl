5864137293a35f35889528fb65b384f96389553a
0oshowero0 <o0shower0o@outlook.com>
2025-11-24 20:30:08 +0800

    fix validate retrieve data batch size
    Signed-off-by: 0oshowero0 <o0shower0o@outlook.com>
    
    fix
    
    Signed-off-by: 0oshowero0 <o0shower0o@outlook.com>

--- recipe/transfer_queue/ray_trainer.py
+++ recipe/transfer_queue/ray_trainer.py
@@ -687,7 +687,7 @@ class RayPPOTrainer:
             batch_meta = asyncio.run(
                 self.data_system_client.async_get_meta(
                     data_fields=["input_ids", "uid", "reward_model"],
-                    batch_size=self.val_batch_size * self.config.actor_rollout_ref.rollout.val_kwargs.n,
+                    batch_size=test_batch.batch_size[0],
                     partition_id=f"val_{self.global_steps - 1}",
                     task_name="get_data",
                 )
@@ -705,7 +705,7 @@ class RayPPOTrainer:
             test_gen_meta = asyncio.run(
                 self.data_system_client.async_get_meta(
                     data_fields=list(test_batch.keys()),  # TODO: (TQ) Get metadata by specified fields
-                    batch_size=self.val_batch_size * self.config.actor_rollout_ref.rollout.val_kwargs.n,
+                    batch_size=test_batch.batch_size[0],
                     partition_id=f"val_{self.global_steps - 1}",  # self.global_steps start from 1
                     task_name="generate_sequences",
                 )
@@ -734,7 +734,7 @@ class RayPPOTrainer:
             test_response_meta = asyncio.run(
                 self.data_system_client.async_get_meta(
                     data_fields=["responses"],
-                    batch_size=self.val_batch_size * self.config.actor_rollout_ref.rollout.val_kwargs.n,
+                    batch_size=test_batch.batch_size[0],
                     partition_id=f"val_{self.global_steps - 1}",  # self.global_steps start from 1
                     task_name="get_response",
                 )
@@ -762,7 +762,7 @@ class RayPPOTrainer:
             val_reward_meta = asyncio.run(
                 self.data_system_client.async_get_meta(
                     data_fields=compute_reward_fields,
-                    batch_size=self.val_batch_size * self.config.actor_rollout_ref.rollout.val_kwargs.n,
+                    batch_size=test_batch.batch_size[0],
                     partition_id=f"val_{self.global_steps - 1}",
                     task_name="compute_reward",
                 )
@@ -785,7 +785,7 @@ class RayPPOTrainer:
                 num_turns_meta = asyncio.run(
                     self.data_system_client.async_get_meta(
                         data_fields=["__num_turns__"],
-                        batch_size=self.val_batch_size * self.config.actor_rollout_ref.rollout.val_kwargs.n,
+                        batch_size=test_batch.batch_size[0],
                         partition_id=f"val_{self.global_steps - 1}",  # self.global_steps start from 1
                         task_name="get_num_turns",
                     )
@@ -798,7 +798,7 @@ class RayPPOTrainer:
                 data_source_meta = asyncio.run(
                     self.data_system_client.async_get_meta(
                         data_fields=["data_source"],
-                        batch_size=self.val_batch_size * self.config.actor_rollout_ref.rollout.val_kwargs.n,
+                        batch_size=test_batch.batch_size[0],
                         partition_id=f"val_{self.global_steps - 1}",  # self.global_steps start from 1
                         task_name="get_data_source",
                     )
