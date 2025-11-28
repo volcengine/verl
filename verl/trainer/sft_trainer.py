8b262b13eb12c211212bd8221420398bda4e8e51
A1waysBeenHere <moyicong1999@163.com>
2025-11-28 09:59:39 +0800

    Implemented VeomniEngine as a alternative training backend

--- verl/trainer/sft_trainer.py
+++ verl/trainer/sft_trainer.py
@@ -109,6 +109,7 @@ class SFTTrainer:
             engine_config=self.engine_config,
             optimizer_config=self.optimizer_config,
             checkpoint_config=self.checkpoint_config,
+            config=self.config,
         )
 
         self.training_client = TrainingWorker(config=config)
