eedc05612bba417eb36fde79d4c8431259e46f82
HappyAngel <chenjiaobuaa@126.com>
2025-12-16 21:03:56 +0800

    fix: fix format according to mr ci

--- verl/workers/rollout/vllm_rollout/vllm_async_server.py
+++ verl/workers/rollout/vllm_rollout/vllm_async_server.py
@@ -261,6 +261,7 @@ class vLLMHttpServerBase:
             enable_sleep_mode = False
             logger.info(f"enable_sleep_mode: {enable_sleep_mode}, set_expandable_segments is True")
             from vllm.attention.selector import set_expandable_segments
+
             set_expandable_segments(True)
 
         quantization = self.config.quantization
