69bcad039d45c99d2f9f5fb8f3280ab76066d3cc
kevssim <kw1315612141@163.com>
2025-12-25 09:20:29 +0800

    lint fix

--- tests/models/test_tiled_mlp_accuracy.py
+++ tests/models/test_tiled_mlp_accuracy.py
@@ -1,4 +1,3 @@
-"""
 # Copyright 2025 Bytedance Ltd. and/or its affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""
 Test script to verify TiledMLP accuracy by comparing logits and gradients
 between regular MLP and TiledMLP under FSDP2.
 Run with: torchrun --nproc_per_node=2 tests/test_tiled_mlp_accuracy.py
