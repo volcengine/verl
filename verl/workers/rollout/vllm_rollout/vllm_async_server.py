7c1420792030fd6bdccc09755281ac84ac0fa3f8
bruceszchen <bruceszchen@tencent.com>
2026-01-06 19:53:09 +0800

    Fix typo.

--- verl/workers/rollout/vllm_rollout/vllm_async_server.py
+++ verl/workers/rollout/vllm_rollout/vllm_async_server.py
@@ -200,7 +200,7 @@ class vLLMHttpServerBase:
 
         self.config: RolloutConfig = omega_conf_to_dataclass(config)
         self.model_config: HFModelConfig = omega_conf_to_dataclass(model_config, dataclass_type=HFModelConfig)
-        self.config.max_model_len = get_max_position_embeddings(self.model_config.hf_config) 
+        self.config.max_model_len = get_max_position_embeddings(self.model_config.hf_config)
         self.rollout_mode = rollout_mode
         self.workers = workers
 
