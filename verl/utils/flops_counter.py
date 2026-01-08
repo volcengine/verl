25937293e6e216dafb61cf9c32a94b717591df3d
zhihaofang1017 <zhihaofang1017@outlook.com>
2026-01-08 11:12:49 +0800

    format code

--- verl/utils/flops_counter.py
+++ verl/utils/flops_counter.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torch
 import inspect
+
+import torch
 from transformers import PretrainedConfig
 
 from verl.utils.device import get_torch_device
