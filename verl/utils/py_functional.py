1d0e23e309b0e3007aff5cef97374891ae859993
JacobHelwig <jacob.a.helwig@gmail.com>
2026-01-07 21:48:48 -0600

    Init list

--- verl/utils/py_functional.py
+++ verl/utils/py_functional.py
@@ -198,7 +198,7 @@ def append_to_dict(data: dict, new_data: dict, prefix: str = ""):
     for key, val in new_data.items():
         new_key = f"{prefix}{key}" if not key.startswith(prefix) else key
         if new_key not in data:
-            data[new_key] = Metric.init_list(val) if isinstance(val, Metric) else []
+            data[new_key] = val.init_list() if isinstance(val, Metric) else []
         if isinstance(val, list):
             data[new_key].extend(val)
         else:
