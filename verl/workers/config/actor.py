c3f980ee2856847286e120e4a5051dc5b249f726
JacobHelwig <jacob.a.helwig@gmail.com>
2026-01-15 21:38:44 -0600

    Distillation cfg

--- verl/workers/config/actor.py
+++ verl/workers/config/actor.py
@@ -25,7 +25,7 @@ from .engine import FSDPEngineConfig, McoreEngineConfig
 from .model import HFModelConfig
 from .optimizer import OptimizerConfig
 
-__all__ = ["PolicyLossConfig", "RouterReplayConfig", "ActorConfig", "FSDPActorConfig", "McoreActorConfig"]
+__all__ = ["PolicyLossConfig", "RouterReplayConfig", "FSDPDistillationConfig", "DistillationConfig", "ActorConfig", "FSDPActorConfig", "McoreActorConfig"]
 
 
 @dataclass
@@ -79,6 +79,20 @@ class PolicyLossConfig(BaseConfig):
     kl_cov_ratio: float = 0.0002
     ppo_kl_coef: float = 0.1
 
+@dataclass
+class DistillationConfig(BaseConfig):
+    """Configuration for distillation training.
+    TODO
+    """
+    loss_mode: str
+    topk: Optional[int]
+    use_policy_loss: bool
+    distillation_loss_coef: float
+
+@dataclass
+class FSDPDistillationConfig(DistillationConfig):
+    """Configuration for distillation training with FSDP."""
+    pass
 
 @dataclass
 class ActorConfig(BaseConfig):
@@ -274,6 +288,7 @@ class FSDPActorConfig(ActorConfig):
             with chunking for memory efficiency.
         entropy_checkpointing (bool): Whether to use gradient checkpointing for entropy computation.
         fsdp_config (dict[str, Any]): Configuration for FSDP settings.
+        distillation_config (FSDPDistillationConfig): Configuration for distillation settings.
         use_remove_padding (bool): Whether to remove padding tokens in inputs during training
     """
 
@@ -283,6 +298,7 @@ class FSDPActorConfig(ActorConfig):
     entropy_from_logits_with_chunking: bool = False
     entropy_checkpointing: bool = False
     fsdp_config: FSDPEngineConfig = field(default_factory=FSDPEngineConfig)
+    distillation_config: FSDPDistillationConfig = field(default_factory=FSDPDistillationConfig)
     use_remove_padding: bool = False
     use_rollout_log_probs: bool = False
     calculate_sum_pi_squared: bool = False
