13c4d3a454e34e4359cffec9b882a32940fd488b
bruceszchen <bruceszchen@tencent.com>
2026-01-16 20:58:41 +0800

    Fix strict_box_verify not being forwarded to DAPO compute_score.

--- verl/experimental/reward_loop/reward_manager/dapo.py
+++ verl/experimental/reward_loop/reward_manager/dapo.py
@@ -33,6 +33,7 @@ class DAPORewardManager(RewardManagerBase):
         overlong_buffer_cfg = config.reward_model.get("reward_kwargs", {}).get("overlong_buffer_cfg", None)
         self.overlong_buffer_cfg = overlong_buffer_cfg
         self.max_resp_len = config.reward_model.get("reward_kwargs", {}).get("max_resp_len", None)
+        self.strict_box_verify = config.reward_model.get("reward_kwargs", {}).get("strict_box_verify", False)
         self.reward_router_address = reward_router_address
         self.reward_model_tokenizer = reward_model_tokenizer
 
@@ -73,6 +74,7 @@ class DAPORewardManager(RewardManagerBase):
                 solution_str=response_str,
                 ground_truth=ground_truth,
                 extra_info=extra_info,
+                strict_box_verify=self.strict_box_verify,
                 **extra_reward_kwargs,
             )
         else:
@@ -83,6 +85,7 @@ class DAPORewardManager(RewardManagerBase):
                     solution_str=response_str,
                     ground_truth=ground_truth,
                     extra_info=extra_info,
+                    strict_box_verify=self.strict_box_verify,
                     **extra_reward_kwargs,
                 ),
             )
