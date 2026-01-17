355e1b84f74dc37503c13eada5c65b191479d77b
JacobHelwig <jacob.a.helwig@gmail.com>
2026-01-17 12:33:30 -0600

    Prompt lens

--- verl/workers/utils/padding.py
+++ verl/workers/utils/padding.py
@@ -135,6 +135,7 @@ def _slice_response_from_unpad_output(tensor: torch.Tensor, data: TensorDict) ->
     sequence_lens = prompt_lens + response_lens
     sequence_offsets = sequence_lens.cumsum(dim=0)
     assert sequence_offsets[-1].item() == values.shape[0]
+    assert not prompt_lens.eq(0).any(), f"seq_offset - resp_len - 1 assumes prompt_len > 0. Got {prompt_lens}"
 
     response_list = []
     # Skip padding dimensions after sequence dimensions, if any.
