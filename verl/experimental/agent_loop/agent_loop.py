4f9b0d7946f91ea782c33b9f956d8c7c9c0e9ed1
MissLittleFish <yhuang@smail.nju.edu.cn>
2026-01-21 14:08:55 +0800

    agent loop metrics fix

--- verl/experimental/agent_loop/agent_loop.py
+++ verl/experimental/agent_loop/agent_loop.py
@@ -1045,7 +1045,7 @@ class AgentLoopManager:
         if self.config.get("transfer_queue", None) and self.config.transfer_queue.get("enable", False):
             attention_mask = self.tq_client.get_data(output[slowest])["attention_mask"]
             # TODO: debug prompt length retrieval.
-            prompt_length = output.samples[0].get_field_by_name("prompts").shape[1]
+            prompt_length = output.samples[0].get_field_by_name("prompts").shape[0]
             # prompt_length = self.tq_client.get_data(output)["prompts"].shape[1]
         else:
             attention_mask = output.batch["attention_mask"][slowest]
