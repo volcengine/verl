74b857a1f9e732513a29d612e59dbb4e1b41c55d
RobotGF <1083760447@qq.com>
2026-01-27 11:18:15 +0800

    set max_model_len by max_model_len or use max_position_embedding

--- verl/workers/rollout/vllm_rollout/vllm_async_server.py
+++ verl/workers/rollout/vllm_rollout/vllm_async_server.py
@@ -109,10 +109,15 @@ class vLLMHttpServer:
 
         self.config: RolloutConfig = omega_conf_to_dataclass(config)
         self.model_config: HFModelConfig = omega_conf_to_dataclass(model_config, dataclass_type=HFModelConfig)
-        self.config.max_model_len = min(
-            get_max_position_embeddings(self.model_config.hf_config),
-            self.config.prompt_length + self.config.response_length,
-        )
+        if self.config.max_model_len is None:
+            self.config.max_model_len = get_max_position_embeddings(self.model_config.hf_config)
+        else:
+            max_position_embeddings = get_max_position_embeddings(self.model_config.hf_config)
+            assert self.config.max_model_len <= max_position_embeddings, (
+                f"max_model_len ({self.config.max_model_len}) should be less than or equal to "
+                f"max_position_embeddings ({max_position_embeddings})"
+            )
+
         self.rollout_mode = rollout_mode
         self.workers = workers
 
