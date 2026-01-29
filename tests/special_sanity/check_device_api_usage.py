dfa8390e157aa451e4f7f95e98d7b82bf3251dce
yezhibei <yezhibei@huawei.com>
2026-01-29 15:39:28 +0800

    api cuda use filter

--- tests/special_sanity/check_device_api_usage.py
+++ tests/special_sanity/check_device_api_usage.py
@@ -46,6 +46,7 @@ CUDA_KEYWORD_CHECK_WHITELIST = [
     "verl/workers/rollout/vllm_rollout/vllm_async_server.py",  # appear in config.cudagraph_capture_sizes
     "verl/workers/rollout/sglang_rollout/async_sglang_server.py",  # manually set CUDA_VISIBLE_DEVICES
     "verl/workers/rollout/trtllm_rollout/trtllm_async_server.py",  # appear in config.cudagraph_capture_sizes
+    "verl/workers/rollout/replica.py",  # appear in default device_name
     "verl/checkpoint_engine",  # checkpoint engine backend are device specific
 ]
 
